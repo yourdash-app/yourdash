5121508de1682ec0197373b64ace3f6f52e3004d
ewsgit <ewsgit@duck.com>
2022-07-10 20:26:03 +0100

    Client: add basic cursor support
    Signed-off-by: ewsgit <ewsgit@duck.com>

--- frontend/components/app/code-editor/TextEditor.tsx
+++ frontend/components/app/code-editor/TextEditor.tsx
@@ -21,105 +21,116 @@
  *   SOFTWARE.
  */
 
-import React, { useState, useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import Tokenize from "./codesense/Tokenizer";
 import Colorize from "./codesense/Colorizer";
-import { TOKEN } from "./codesense/Typings";
+import {TOKEN} from "./codesense/Typings";
 import getCharacterWidth from "../../../lib/getCharacterWidth";
 
 export default function TextEditor() {
-  const [code, setCode] = useState([
-    '(this is a test) === "aaa" "ass sss aaa" `hmmm mmmm mmmm mmm`  bbb this is a test',
-    "and this is a function aaa()",
-  ]);
-  const [tokenizedCode, setTokenizedCode] = useState([] as TOKEN[][]);
-  const [linesOfCode, setLinesOfCode] = useState([] as number[]);
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 } as { x: number; y: number });
-  let pressedKeys: string[] = [];
-  useEffect(() => {
-    setTokenizedCode(Colorize(Tokenize(code, "js")));
-    setLinesOfCode(code.map((str, ind) => ind));
-  }, [code]);
-  let onKeyDownEvent = (e: KeyboardEvent) => {
-    e.preventDefault();
-    if (pressedKeys.indexOf(e.key) === -1) {
-      pressedKeys.push(e.key);
-    }
-    onKeyPress();
-  };
-  let onKeyUpEvent = (e: KeyboardEvent) => {
-    if (pressedKeys.indexOf(e.key) !== -1) {
-      pressedKeys.splice(pressedKeys.indexOf(e.key), 1);
-    }
-  };
-  let onKeyPress = () => {
-    pressedKeys.map(key => {
-      switch (key) {
-        case "ArrowUp":
-          setCursorPosition({ ...cursorPosition, y: cursorPosition.y-- });
-          break;
-        case "ArrowDown":
-          setCursorPosition({ ...cursorPosition, y: cursorPosition.y++ });
-          break;
-        case "ArrowLeft":
-          setCursorPosition({ ...cursorPosition, x: cursorPosition.x-- });
-          break;
-        case "ArrowRight":
-          setCursorPosition({ ...cursorPosition, x: cursorPosition.x++ });
-          break;
-      }
-    });
-  };
-  return (
-    <div className={`w-full h-full grid grid-cols-[auto,1fr] font-["jetbrains_mono"] relative`}>
-      <div className={`pr-2 pl-2 pt-2 bg-content-normal shadow-md z-10 text-text-secondary select-none`}>
-        {linesOfCode.map(line => {
-          return <div key={line}>{line + 1}</div>;
-        })}
-      </div>
-      <div className="bg-content-light pt-2 pl-2 w-full h-full">
+    const [code, setCode] = useState(['(this is a test) === "aaa" "ass sss aaa" `hmmm mmmm mmmm mmm`  bbb this is a test', "and this is a function aaa() r",]);
+    const [tokenizedCode, setTokenizedCode] = useState([] as TOKEN[][]);
+    const [linesOfCode, setLinesOfCode] = useState([] as number[]);
+    const [cursorPosition, setCursorPosition] = useState({x: 0, y: 0} as { x: number; y: number });
+    const [fontConfig, setFontConfig] = useState({
+        fontSize: 18,
+        lineHeight: 18 * 1.2,
+        fontFamily: "Jetbrains Mono"
+    })
+    let pressedKeys: string[] = [];
+    useEffect(() => {
+        setTokenizedCode(Colorize(Tokenize(code, "js")));
+        setLinesOfCode(code.map((str, ind) => ind));
+    }, [code]);
+    let onKeyDownEvent = (e: KeyboardEvent) => {
+        e.preventDefault()
+        if (pressedKeys.indexOf(e.key) === -1) {
+            pressedKeys.push(e.key);
+        }
+        onKeyPress();
+    };
+    let onKeyUpEvent = (e: KeyboardEvent) => {
+        e.preventDefault()
+        if (pressedKeys.indexOf(e.key) !== -1) {
+            pressedKeys.splice(pressedKeys.indexOf(e.key), 1);
+        }
+    };
+    let onKeyPress = () => {
+        pressedKeys.map(key => {
+            switch (key) {
+                case "ArrowUp":
+                    setCursorPosition({...cursorPosition, y: cursorPosition.y - fontConfig.lineHeight});
+                    break;
+                case "ArrowDown":
+                    setCursorPosition({...cursorPosition, y: cursorPosition.y + fontConfig.lineHeight});
+                    break;
+                case "ArrowLeft":
+                    setCursorPosition({
+                        ...cursorPosition,
+                        x: cursorPosition.x - getCharacterWidth(fontConfig.fontFamily, fontConfig.fontSize + "px")
+                    });
+                    break;
+                case "ArrowRight":
+                    setCursorPosition({
+                        ...cursorPosition,
+                        x: cursorPosition.x + getCharacterWidth(fontConfig.fontFamily, fontConfig.fontSize + "px")
+                    });
+                    break;
+            }
+        });
+    };
+    return (<div className={`w-full h-full grid grid-cols-[auto,1fr] relative`}>
+        <div className={`pr-2 pl-2 pt-2 bg-content-normal shadow-md z-10 text-text-secondary select-none`}>
+            {linesOfCode.map(line => {
+                return <div style={{
+                    fontSize: fontConfig.fontSize + "px",
+                    lineHeight: fontConfig.lineHeight + "px",
+                    fontFamily: fontConfig.fontFamily
+                }} key={line}>{line + 1}</div>;
+            })}
+        </div>
+        <div className="bg-content-light pt-2 pl-2 w-full h-full overflow-scroll">
         <pre
-          className={`relative overflow-scroll cursor-text w-full h-full`}
-          tabIndex={0}
-          onFocus={e => {
-            e.target.addEventListener("keydown", onKeyDownEvent);
-            e.target.addEventListener("keyup", onKeyUpEvent);
-          }}
-          onBlur={e => {
-            e.target.removeEventListener("keydown", onKeyDownEvent);
-            e.target.removeEventListener("keyup", onKeyUpEvent);
-          }}
-          onMouseMove={e => {
-            e.preventDefault();
-            setCursorPosition({
-              x:
-                Math.round(
-                  (e.clientX - e.currentTarget.getClientRects()[0].x) / getCharacterWidth("0", "jetbrains_mono 24px")
-                ) * getCharacterWidth("0", "jetbrains_mono 24px"),
-              y: Math.floor((e.clientY - e.currentTarget.getClientRects()[0].y) / 24) * 24,
-            });
-          }}>
+            className={`relative overflow-scroll cursor-text w-full h-full`}
+            tabIndex={0}
+            onFocus={e => {
+                e.target.addEventListener("keydown", onKeyDownEvent);
+                e.target.addEventListener("keyup", onKeyUpEvent);
+            }}
+            onBlur={e => {
+                e.target.removeEventListener("keydown", onKeyDownEvent);
+                e.target.removeEventListener("keyup", onKeyUpEvent);
+            }}
+            onClick={e => {
+                e.preventDefault();
+                setCursorPosition({
+                    x: Math.round((e.clientX - e.currentTarget.getClientRects()[0].x) / getCharacterWidth(fontConfig.fontFamily, fontConfig.fontSize + "px")) * getCharacterWidth(fontConfig.fontFamily, fontConfig.fontSize + "px"),
+                    y: Math.floor((e.clientY - e.currentTarget.getClientRects()[0].y) / fontConfig.lineHeight) * fontConfig.lineHeight,
+                });
+            }}
+        >
           {tokenizedCode.map((line, ind) => {
-            return (
-              <span key={ind} className={`flex`}>
+              return (<span key={ind} className={`flex`}>
                 {line.map((token, tokenInd) => {
-                  return (
-                    <span key={tokenInd} style={{ color: token.color, fontWeight: token.bold ? "bold" : "normal" }}>
+                    return (<span key={tokenInd} style={{
+                        color: token.color,
+                        fontWeight: token.bold ? "bold" : "normal",
+                        fontSize: fontConfig.fontSize + "px",
+                        lineHeight: fontConfig.lineHeight + "px",
+                        fontFamily: fontConfig.fontFamily
+                    }}>
                       {token.value}
-                    </span>
-                  );
+                    </span>);
                 })}
-              </span>
-            );
+              </span>);
           })}
-          <div
-            className={`w-0.5 h-[24px] bg-branding-primary absolute top-0 left-0 animate-pulse`}
-            style={{
-              left: `${cursorPosition.x}px`,
-              top: `${cursorPosition.y}px`,
-            }}></div>
+            <div
+                className={`w-0.5 bg-branding-primary absolute top-0 left-0 animate-pulse`}
+                style={{
+                    height: fontConfig.lineHeight + "px",
+                    left: `${cursorPosition.x}px`, top: `${cursorPosition.y}px`,
+                }}></div>
         </pre>
-      </div>
-    </div>
-  );
+        </div>
+    </div>);
 }
