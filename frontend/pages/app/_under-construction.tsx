3acd7601eda73a97bfd969ec3b586e7c1358908d
ewsgit <ewsgit@duck.com>
2022-07-10 20:19:26 +0100

    Client: fix warning line width in _under-construction.tsx
    Signed-off-by: ewsgit <ewsgit@duck.com>

--- frontend/pages/app/_under-construction.tsx
+++ frontend/pages/app/_under-construction.tsx
@@ -26,9 +26,11 @@ import React from "react"
 export default function UnderConstruction() {
   return (
     <div
-      className={`[background-image:repeating-linear-gradient(45deg,rgb(220,190,0)_0%,rgb(0,0,0)_5%);] w-full min-h-full flex items-center justify-center flex-col relative`}>
-      <h1 className={`text-center text-white text-6xl drop-shadow-[0.25rem_0.25rem_0_#000] z-10 mb-2`}>Under Construction</h1>
-      <h2 className={`text-center text-white text-2xl bg-content-normal p-2 rounded-md z-10`}>The current page is under construction...</h2>
+        className={`[background-image:repeating-linear-gradient(45deg,rgb(220,190,0)_0px,rgb(0,0,0)_50px);] w-full min-h-full flex items-center justify-center flex-col relative select-none`}>
+        <h1 className={`text-center text-white text-6xl drop-shadow-[0.25rem_0.25rem_0_#000] z-10 mb-2`}>Under
+            Construction</h1>
+        <h2 className={`text-center text-white text-2xl bg-content-normal p-2 rounded-md z-10 shadow-2xl`}>The current
+            page is under construction...</h2>
         <div className="w-full h-full bg-slate-600 bg-opacity-60 absolute top-0 left-0"></div>
     </div>
   );
