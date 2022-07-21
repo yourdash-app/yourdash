bef11b2a95de4e197abe7b0f0ea077df631c46f1
ewsgit <ewsgit@duck.com>
2022-07-21 18:58:47 +0100

    Frontend: continue to add to apiInterface.ts

--- frontend/lib/apiInterface.ts
+++ frontend/lib/apiInterface.ts
@@ -6,7 +6,7 @@
 
 import * as localforage from "localforage";
 
-interface UserTypings {
+interface GithubUserTypings {
     avatar_url: string,
     bio: string,
     blog: string | null,
@@ -44,16 +44,16 @@ interface UserTypings {
     url: string
 }
 
-class User {
-    userData: UserTypings
+class GithubUser {
+    userData: GithubUserTypings
 
-    constructor(userData: UserTypings) {
+    constructor(userData: GithubUserTypings) {
         this.userData = userData
         return this
     }
 
-    getName(): string {
-        return this.userData.name
+    getUsername(): string {
+        return this.userData.login
     }
 
     getAvatar(): string {
@@ -72,7 +72,7 @@ class User {
         return this.userData.company
     }
 
-    getCreationTime(): Date {
+    getCreationDate(): Date {
         return new Date(this.userData.created_at)
     }
 
@@ -81,12 +81,12 @@ class User {
     }
 
     // @ts-ignore
-    getEvents(): Event[] {
+    getEvents(): GithubEvent[] | null {
         fetch(this.userData.events_url)
             .then(res => res.json())
             .then(res => {
                 return [...res.map((e: any) => {
-                    return new Event(e)
+                    return new GithubEvent(e)
                 })]
             })
             .catch(e => {
@@ -94,30 +94,117 @@ class User {
                 return null
             })
     }
+
+    // @ts-ignore
+    getFollowers(): GithubFollower[] | null {
+        fetch(this.userData.followers_url)
+            .then(res => res.json())
+            .then(res => {
+                return [...res.map((e: any) => {
+                    return new GithubFollower(e)
+                })]
+            })
+            .catch(e => {
+                console.error(e)
+                return null
+            })
+    }
+
+    getName(): string {
+        return this.userData.name
+    }
 }
 
-interface EventTypings {
+class GithubFollower {
+    follower: GithubFollowerTypings
+    constructor(follower: GithubFollowerTypings) {
+        this.follower = follower
+        return this
+    }
+
+    getUsername(): string {
+        return this.follower.login
+    }
+
+    getId(): number {
+        return this.follower.id
+    }
+
+    getNodeId(): string {
+        return this.follower.node_id
+    }
+
+    getAvatar(): string {
+        return this.follower.avatar_url
+    }
+
+    getGravatarId(): string {
+        return this.follower.gravatar_id
+    }
+
+    // @ts-ignore
+    getUser(): GithubUser | null {
+        fetch(this.follower.url)
+            .then(req => req.json())
+            .then(user => {
+                return new GithubUser(user)
+            })
+            .catch(e => {
+                console.error(e)
+                return null
+            })
+    }
+
+    isGithubAdmin(): boolean {
+        return this.follower.site_admin
+    }
+}
+
+interface GithubFollowerTypings {
+    login: string,
+    id: number,
+    node_id: string,
+    avatar_url: string,
+    gravatar_id: string,
+    url: string,
+    html_url: string,
+    followers_url: string,
+    following_url: string,
+    gists_url: string,
+    starred_url: string,
+    subscriptions_url: string,
+    organizations_url: string,
+    repos_url: string,
+    events_url: string,
+    received_events_url: string,
+    type: "User",
+    site_admin: boolean
+}
+
+interface GithubEventTypings {
     id: string,
-    type: "WatchEvent" | "PushEvent", // FIXME: add other varients
+    type: "WatchEvent" | "PushEvent", // FIXME: add other variants
     actor: {
         id: number, login: string, display_login: string, gravatar_id: string, url: string, avatar_url: string
     },
     repo: { id: number, name: string, url: string },
     payload: {
-        action: "started" // FIXME: add other varients
+        action: "started" // FIXME: add other variants
     },
     public: boolean,
     created_at: string
 }
 
-class Event {
-    constructor(eventData: object[]) {
+class GithubEvent {
+    event: GithubEventTypings
+    constructor(eventData: GithubEventTypings) {
+        this.event = eventData
         return this
     }
 }
 
 // @ts-ignore
-export default function GetUser(): User {
+export default function GetCurrentUser(): User {
     localforage.getItem("githubUser").then(data => {
         return data
     }).catch(e => {
