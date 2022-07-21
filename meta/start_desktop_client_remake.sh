49e32be7ed0a9d1d6d2723dbd0b702b26eec6f98
ewsgit <ewsgit@duck.com>
2022-07-21 18:58:22 +0100

    Desktop Client: fix the naming of tauri

--- meta/start_desktop_client_remake.sh
+++ meta/start_desktop_client_remake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-npx kill-port 3000 3001 3002 3003 3004 3005
 cd ./frontend/
+npx kill-port 3000 3001 3002 3003 3004 3005
 npm run dev &
 cd ./../desktop-client/tauri-remake/
 npm run tauri dev
