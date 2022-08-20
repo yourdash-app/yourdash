f66d1b30d2a151c85e4684c11f2088ddb17c0ec8
ewsgit <ewsgit@duck.com>
2022-08-20 13:10:20 +0100

    Auth: add copyright notice and update "/" response

--- backend/auth/index.ts
+++ backend/auth/index.ts
@@ -1,3 +1,26 @@
+/*
+ *   Copyright (c) 2022 Ewsgit
+ *   All rights reserved.
+
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy
+ *   of this software and associated documentation files (the "Software"), to deal
+ *   in the Software without restriction, including without limitation the rights
+ *   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *   copies of the Software, and to permit persons to whom the Software is
+ *   furnished to do so, subject to the following conditions:
+ 
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+ 
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *   SOFTWARE.
+ */
+
 import express from "express";
 import fetch from "node-fetch";
 import cors from "cors";
@@ -13,7 +36,7 @@ app.use(cors());
 // online test
 app.use(cookieParser());
 app.get("/", (req, res) => {
-  res.send("Hello World! - DevDash Api");
+  res.send("Hello World! - DevDash Auth Api.");
 });
 
 // GitHub auth
