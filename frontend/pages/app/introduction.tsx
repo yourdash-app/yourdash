2828966715365636dc77b236b91725c62fea1b4f
ewsgit <ewsgit@duck.com>
2022-08-23 07:56:28 +0100

    Frontend: introduction add set theme logic

--- frontend/pages/app/introduction.tsx
+++ frontend/pages/app/introduction.tsx
@@ -21,62 +21,151 @@
  *   SOFTWARE.
  */
 
-import React, { useState } from "react"
+import React, { useState } from 'react';
+import setTheme from '../../lib/setTheme';
 
 export default function IntroductionPage() {
-  const [ currentPage, setCurrentPage ] = useState(0)
-  return <div className={"w-screen h-screen bg-bg-light-secondary dark:bg-bg-dark-secondary flex items-center justify-center"}>
-    {
-      currentPage === 0
-        ? <SubPage>
-          <h1 className={"text-text-primary text-5xl text-center mb-4 mt-4 select-none"}>Choose a theme</h1>
-          <div className={"w-[calc(100%-2rem)] grid grid-cols-3 h-[calc(100%-6rem)] gap-2 ml-4 mr-4 place-items-center"}>
-            <ThemeSelectionCard theme="light" image={require("./../../assets/homescreenWave.svg").default.src} />
-            <ThemeSelectionCard theme="auto" image={require("./../../assets/homescreenWave.svg").default.src} />
-            <ThemeSelectionCard theme="dark" image={require("./../../assets/homescreenWave.svg").default.src} />
+  const [currentPage, setCurrentPage] = useState(0);
+  const [themeSelected, setThemeSelected] = useState(
+    'auto' as 'light' | 'auto' | 'dark'
+  );
+  return (
+    <div
+      className={
+        'w-screen h-screen transition-[background-color] bg-bg-light-secondary flex items-center justify-center'
+      }>
+      {currentPage === 0 ? (
+        <SubPage>
+          <h1
+            className={
+              'text-text-primary text-5xl text-center mb-4 mt-4 select-none'
+            }>
+            Choose a theme
+          </h1>
+          <div
+            className={
+              'w-[calc(100%-2rem)] grid grid-cols-1 h-[calc(100%-6rem)] gap-2 ml-4 mr-4 place-items-center sm:grid-cols-3'
+            }>
+            <ThemeSelectionCard
+              currentlySelectedTheme={themeSelected}
+              theme='light'
+              onClick={() => {
+                setThemeSelected('light');
+                localStorage.setItem('themeMode', 'light');
+                setTheme();
+              }}
+              image={require('./../../assets/homescreenWave.svg').default.src}
+            />
+            <ThemeSelectionCard
+              currentlySelectedTheme={themeSelected}
+              theme='auto'
+              onClick={() => {
+                setThemeSelected('auto');
+                localStorage.setItem('themeMode', 'system');
+                setTheme();
+              }}
+              image={require('./../../assets/homescreenWave.svg').default.src}
+            />
+            <ThemeSelectionCard
+              currentlySelectedTheme={themeSelected}
+              theme='dark'
+              onClick={() => {
+                setThemeSelected('dark');
+                localStorage.setItem('themeMode', 'dark');
+                setTheme();
+              }}
+              image={require('./../../assets/homescreenWave.svg').default.src}
+            />
           </div>
         </SubPage>
-        : null
-    }
-    <PageIndicatorContainer currentPage={currentPage} pageCount={5} setCurrentPage={setCurrentPage} />
-  </div>
+      ) : null}
+      <PageIndicatorContainer
+        currentPage={currentPage}
+        pageCount={5}
+        setCurrentPage={setCurrentPage}
+      />
+    </div>
+  );
 }
 
-function SubPage(props: { children: React.ReactChild | React.ReactChild[], className?: string }) {
-  return <div className={"w-5/6 h-full max-h-[calc(100vh-6rem)] bg-content-normal rounded-3xl child:animate-fade-in child:opacity-0 " + props.className}>
-    {props.children}
-  </div>
+function SubPage(props: {
+  children: React.ReactChild | React.ReactChild[];
+  className?: string;
+}) {
+  return (
+    <div
+      className={
+        'w-full md:rounded-3xl md:w-5/6 h-full max-h-[calc(100vh-6rem)] bg-content-normal child:animate-fade-in child:opacity-0 transition-[width] ' +
+        props.className
+      }>
+      {props.children}
+    </div>
+  );
 }
 
-function PageIndicatorContainer(props: { currentPage: number, pageCount: number, setCurrentPage: (page: number) => void }) {
-  let out = []
+function PageIndicatorContainer(props: {
+  currentPage: number;
+  pageCount: number;
+  setCurrentPage: (page: number) => void;
+}) {
+  let out = [];
   for (let i = 0; i < props.pageCount; i++) {
     out.push(
       <div
         key={i}
-        className={`p-1.5 pl-2.5 pr-2.5 rounded-full last:mr-0 first:ml-0 mr-1.5 ml-1.5 ${props.currentPage === i ? "bg-branding-primary" : "bg-content-dark"} hover:scale-150 hover:outline-content-border outline-2 hover:outline transition-all cursor-pointer animate-fade-in opacity-0`}
+        className={`p-1.5 pl-2.5 pr-2.5 rounded-full last:mr-0 first:ml-0 mr-1.5 ml-1.5 ${
+          props.currentPage === i ? 'bg-branding-primary' : 'bg-content-dark'
+        } hover:scale-150 hover:outline-content-border outline-2 hover:outline transition-all cursor-pointer animate-fade-in opacity-0`}
         onClick={() => {
-          props.setCurrentPage(i)
-        }}
-      ></div>
-    )
+          props.setCurrentPage(i);
+        }}></div>
+    );
   }
-  return <div className={"-translate-x-1/2 fixed bottom-2 left-1/2 flex items-center justify-center"}>
-    <div className={"rounded-full bg-content-normal animate-expand-in-horizontal p-2.5 pl-3 pr-3 flex items-center justify-center overflow-hidden"}>
-      {
-        out
-      }
+  return (
+    <div
+      className={
+        '-translate-x-1/2 fixed bottom-2 left-1/2 flex items-center justify-center'
+      }>
+      <div
+        className={
+          'rounded-full bg-content-normal animate-expand-in-horizontal p-2.5 pl-3 pr-3 flex items-center justify-center overflow-hidden'
+        }>
+        {out}
+      </div>
     </div>
-  </div>
+  );
 }
 
-function ThemeSelectionCard(props: { theme: "light" | "auto" | "dark", image: string }) {
-  return <div className={"flex items-center justify-center w-full bg-red-400 overflow-hidden relative h-full max-h-96 max-w-lg rounded-xl child:select-none"}>
-    <img draggable={false} src={props.image} alt="" className="w-full h-full absolute top-0 left-0" />
-    <div className={"flex items-center justify-center w-full absolute bottom-0 left-0 bg-content-normal bg-opacity-75 pt-2 pb-2"}>
-      <h3 className={"m-0 text-text-secondary text-2xl text-center"}>{props.theme}</h3>
+function ThemeSelectionCard(props: {
+  theme: 'light' | 'auto' | 'dark';
+  image: string;
+  onClick: () => void;
+  currentlySelectedTheme: 'light' | 'auto' | 'dark';
+}) {
+  return (
+    <div
+      onClick={props.onClick}
+      className={`flex items-center justify-center transition-all w-full bg-red-400 overflow-hidden relative h-full max-h-96 max-w-lg rounded-xl child:select-none ${
+        props.theme === props.currentlySelectedTheme
+          ? 'outline-4 outline outline-branding-primary'
+          : 'outline-0'
+      }`}>
+      <img
+        draggable={false}
+        src={props.image}
+        alt=''
+        className='w-full h-full absolute top-0 left-0'
+      />
+      <div
+        className={
+          'flex items-center justify-center w-full absolute bottom-0 left-0 bg-content-normal bg-opacity-75 pt-2 pb-2'
+        }>
+        <h3 className={'m-0 text-text-secondary text-2xl text-center'}>
+          {props.theme}
+        </h3>
+      </div>
     </div>
-  </div>
+  );
 }
 
-IntroductionPage.ignoreTemplate = true
+IntroductionPage.ignoreTemplate = true;
