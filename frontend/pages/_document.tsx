b7bb8183be60c1c69226244544954dc5226ce687
ewsgit <ewsgit@duck.com>
2022-08-24 20:46:13 +0100

    Frontend: add default favicon to _document.tsx

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -14,7 +14,8 @@ export default function Document() {
                 <meta name="google-site-verification" content="7cX4pphIkO6E0BneERfoBUoG9q9noi3z-1e8G6XZ3BI"/>
                 <link rel="manifest" href="/manifest.json"/>
                 <link rel="apple-touch-icon" href="/DevDash@2x.png"/>
-                <meta name="apple-mobile-web-app-status-bar" content="#363636"/>
+                <meta name="apple-mobile-web-app-status-bar" content="#363636" />
+                <link rel="shortcut icon" href="/DevDash.png" type="image/x-icon" />
             </Head>
             <body>
             <Main/>
