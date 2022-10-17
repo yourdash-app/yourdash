1a4f782d8c9b39479ad3c84a0c4df37c11b4461e
ewsgit <ewsgit@duck.com>
2022-10-17 09:19:45 +0100

    feat: add section 2 to the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,41 +27,57 @@ import HomeLayout from '../components/layouts/homeLayout/HomeLayout';
 import { NextPageWithLayout } from './page';
 import Button from '../components/elements/button/Button';
 import Slides from '../components/elements/slides/Slides';
+import { useRouter } from 'next/router';
 
 const Home: NextPageWithLayout = () => {
-  return (
-    <div className={styles.root}>
-      <section className={styles.section1}>
-        <div>
-          <div>
-            <Slides slides={[
-              <h1 key={0}>This is coming soon.</h1>,
-              <h1 key={1}>This is coming soon..</h1>,
-              <h1 key={2}>This is coming soon...</h1>,
-              <h1 key={3}>This is coming soon....</h1>,
-              <h1 key={4}>This is coming soon.....</h1>,
-              <h1 key={5}>This is coming soon.....</h1>,
-              <h1 key={6}>This is coming soon.....</h1>,
-              <h1 key={7}>This is coming soon.....</h1>,
-              <h1 key={8}>This is coming soon.....</h1>
-            ]} changeDuration={500} />
-          </div>
+    const router = useRouter()
+    return (
+        <div className={styles.root}>
+            <section className={styles.section1}>
+                <div>
+                    <div>
+                        <Slides slides={[
+                            <h1 key={0}>This is coming soon.</h1>,
+                            <h1 key={1}>This is coming soon..</h1>,
+                            <h1 key={2}>This is coming soon...</h1>,
+                            <h1 key={3}>This is coming soon....</h1>,
+                            <h1 key={4}>This is coming soon.....</h1>,
+                            <h1 key={5}>This is coming soon.....</h1>,
+                            <h1 key={6}>This is coming soon.....</h1>,
+                            <h1 key={7}>This is coming soon.....</h1>,
+                            <h1 key={8}>This is coming soon.....</h1>
+                        ]} changeDuration={1000} />
+                    </div>
+                </div>
+                <div>
+                    <h1>Your Dash</h1>
+                    <p>The home for your files.</p>
+                    <div>
+                        <Button onClick={() => { router.push("/login/server") }} vibrant>Sign Up</Button>
+                        <Link href="/login/server">Login</Link>
+                    </div>
+                </div>
+            </section>
+            <section className={styles.section2}>
+                <div>
+                    <h2>Placeholder Text</h2>
+                    <button>Learn more</button>
+                </div>
+                <div>
+                    <h2>Placeholder Text</h2>
+                    <button>Learn more</button>
+                </div>
+                <div>
+                    <h2>Placeholder Text</h2>
+                    <button>Learn more</button>
+                </div>
+            </section>
         </div>
-        <div>
-          <h1>Your Dash</h1>
-          <p>The home for your files.</p>
-          <div>
-            <Button vibrant>Sign Up</Button>
-            <Link href="#">Login</Link>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+    );
 };
 
 export default Home;
 
 Home.getLayout = (page) => {
-  return <HomeLayout>{page}</HomeLayout>
+    return <HomeLayout>{page}</HomeLayout>
 }
