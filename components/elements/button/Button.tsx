801e99cc28a083958f6ca44936bfb936949954e6
ewsgit <ewsgit@duck.com>
2022-10-20 09:02:43 +0100

    feat: add disabled ButtonComponent prop

--- components/elements/button/Button.tsx
+++ components/elements/button/Button.tsx
@@ -27,10 +27,11 @@ export interface IButton {
   children: React.ReactChild | React.ReactChild[];
   vibrant?: boolean;
   onClick: () => void;
+  disabled?: boolean;
 }
 
-const Button: React.FC<IButton> = ({ children, vibrant, onClick }) => {
-  return <button onClick={onClick} className={`${styles.component} ${vibrant ? styles.vibrant : ""}`}>{children}</button>;
+const Button: React.FC<IButton> = ({ children, vibrant, onClick, disabled }) => {
+  return <button onClick={() => { if (!disabled) onClick() }} className={`${styles.component} ${vibrant ? styles.vibrant : ""}`}>{children}</button>;
 };
 
 export default Button;
