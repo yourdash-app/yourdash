50d2f5e61e38f99a8656d22ee647c8a529234ca9
ewsgit <ewsgit@duck.com>
2022-11-07 20:57:25 +0000

    general: move the server library to exporting separate functions

--- lib/server.ts
+++ lib/server.ts
@@ -4,49 +4,46 @@
  * Copyright © 2022 Ewsgit
  */
 
-const Server = {
-  get: (path: string): Promise<Response> => {
-    let defaultHeaders = {
-      userToken: localStorage.getItem('token') as string,
-      userName: localStorage.getItem('userName') as string,
-    };
-    let url = localStorage.getItem('currentServer');
-    console.log('[Server Request]: GET ' + path);
-    return new Promise((resolve, reject) => {
-      fetch(`${url}/api${path}`, {
-        headers: { ...defaultHeaders },
-        method: 'GET',
-      })
-        .then((res) => resolve(res))
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  },
-  post: (path: string): Promise<Response> => {
-    let defaultHeaders = {
-      userToken: localStorage.getItem('token') as string,
-      userName: localStorage.getItem('userName') as string,
-    };
-    let url = localStorage.getItem('currentServer');
-    console.log('[Server Request]: POST ' + path);
-    return fetch(`${url}/api${path}`, {
+export function getServer(path: string): Promise<Response> {
+  console.log('[Server Request]: GET ' + path);
+  console.trace();
+  let defaultHeaders = {
+    userToken: localStorage.getItem('token') as string,
+    userName: localStorage.getItem('userName') as string,
+  };
+  let url = localStorage.getItem('currentServer');
+  return new Promise((resolve, reject) => {
+    fetch(`${url}/api${path}`, {
       headers: { ...defaultHeaders },
-      method: 'POST',
-    });
-  },
-  delete: (path: string): Promise<Response> => {
-    let defaultHeaders = {
-      userToken: localStorage.getItem('token') as string,
-      userName: localStorage.getItem('userName') as string,
-    };
-    let url = localStorage.getItem('currentServer');
-    console.log('[Server Request]: DELETE ' + path);
-    return fetch(`${url}/api${path}`, {
-      headers: { ...defaultHeaders },
-      method: 'DELETE',
-    });
-  },
-};
-
-export default Server;
+      method: 'GET',
+    })
+      .then((res) => resolve(res))
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
+export function postServer(path: string): Promise<Response> {
+  let defaultHeaders = {
+    userToken: localStorage.getItem('token') as string,
+    userName: localStorage.getItem('userName') as string,
+  };
+  let url = localStorage.getItem('currentServer');
+  console.log('[Server Request]: POST ' + path);
+  return fetch(`${url}/api${path}`, {
+    headers: { ...defaultHeaders },
+    method: 'POST',
+  });
+}
+export function deleteServer(path: string): Promise<Response> {
+  let defaultHeaders = {
+    userToken: localStorage.getItem('token') as string,
+    userName: localStorage.getItem('userName') as string,
+  };
+  let url = localStorage.getItem('currentServer');
+  console.log('[Server Request]: DELETE ' + path);
+  return fetch(`${url}/api${path}`, {
+    headers: { ...defaultHeaders },
+    method: 'DELETE',
+  });
+}
