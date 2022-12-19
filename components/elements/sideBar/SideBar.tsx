1e14fb641260c87bb62c7f85ee20cf93f11898e7
ewsgit <ewsgit@duck.com>
2022-12-19 11:40:00 +0000

    general: fix sidebar

--- components/elements/sideBar/SideBar.tsx
+++ components/elements/sideBar/SideBar.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Card from '../../containers/card/Card';
 import Button from '../button/Button';
 import styles from './SideBar.module.scss';
+import Icon from '../icon/Icon';
 
 interface IButton {
   title: string,
@@ -24,20 +25,20 @@ const SideBar: React.FC<ISideBar> = ({ title, sections }) => {
     <h2>{title}</h2>
     {
       sections.map((section, ind) => {
-        return <Card className={styles.card} key={ind} data-toggled={toggledSections.indexOf(section.title) !== -1} onClick={() => {
-          console.log(toggledSections)
-          console.log(section.title)
-          if (toggledSections.indexOf(section.title) === -1) {
-            console.log(1)
-            setToggledSections([ ...toggledSections, section.title ])
-          } else {
-            console.log(2)
-            setToggledSections([ ...toggledSections.filter((sect) => sect !== section.title) ])
-          }
-        }}>
+        return <Card className={styles.card} key={ind} data-toggled={toggledSections.indexOf(section.title) !== -1}>
           <>
-            {
-              section.title ? <h3>{section.title}</h3> : null}
+            <section onClick={() => {
+              if (toggledSections.indexOf(section.title) === -1) {
+                setToggledSections([ ...toggledSections, section.title ])
+              } else {
+                setToggledSections([ ...toggledSections.filter((sect) => sect !== section.title) ])
+              }
+            }}>
+              {
+                section.title ? <h3>{section.title}</h3> : null
+              }
+              <Icon data-toggled={toggledSections.indexOf(section.title) !== -1} name='chevron-up-16' color="var(--card-fg)" />
+            </section>
             {
               toggledSections.indexOf(section.title) !== -1 ?
                 section.buttons.map((button, ind) => {
