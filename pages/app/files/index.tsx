85825d24a88483f82c4cdaadbfa8db1b260395b2
Ewsgit <ewsgit@duck.com>
2023-01-21 13:35:17 +0000

    fix: store animations

--- pages/app/files/index.tsx
+++ pages/app/files/index.tsx
@@ -4,21 +4,34 @@ import AppLayout from '../../../components/layouts/appLayout/AppLayout';
 import { NextPageWithLayout } from '../../page';
 import FilesLayout from "./components/FilesLayout";
 import File from "./components/File"
+import filesDirectory from "../../../types/files/directory";
+import filesFile from "../../../types/files/file";
 
 const Files: NextPageWithLayout = () => {
+  const [ items, setItems ] = React.useState([
+    {
+      items: [],
+      path: "/app/",
+      type: "file",
+      name: ""
+    }
+  ] as (filesFile | filesDirectory)[])
+
   return (
     <>
       <Head>
         <title>YourDash | Files</title>
       </Head>
-      <File name="FileName" path="/file/path" type="file" />
-      <File name="FileName" path="/file/path" type="folder" />
-      <File name="FileName" path="/file/path" type="file" />
-      <File name="FileName" path="/file/path" type="file" />
-      <File name="FileName" path="/file/path" type="folder" />
-      <File name="FileName" path="/file/path" type="file" />
-      <File name="FileName" path="/file/path" type="file" />
-      <File name="FileName" path="/file/path" type="file" />
+      {
+        items.map((item) => {
+          switch (item.type) {
+            case "file":
+              return <File name="FileName" path="/file/path" type="file"/>
+            case "directory":
+              return <File name="FileName" path="/file/path" type="folder"/>
+          }
+        })
+      }
     </>
   );
 };
