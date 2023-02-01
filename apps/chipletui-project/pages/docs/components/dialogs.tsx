a1cb5a29c7411c624bcbc40c81e4f42953fc4b42
ewsgit <ewsgit@duck.com>
2023-02-01 13:31:04 +0000

    feat: complete new sidebar implementation

--- apps/chipletui-project/pages/docs/components/dialogs.tsx
+++ apps/chipletui-project/pages/docs/components/dialogs.tsx
@@ -2,27 +2,34 @@ import { NextPageWithLayout } from "../../page";
 import DocsLayout from '../../../layouts/docsLayout/DocsLayout';
 import HomeLayout from '../../../layouts/homeLayout/HomeLayout';
 import Dialog from "ui/dialogs/Dialog";
+import { useState } from "react";
 
-const Docs: NextPageWithLayout = () => (
-  <>
-    <section data-header="true">
-      <img src={"/background.jpg"} alt=""/>
-    </section>
-    <main>
-      <h1>Dialogs</h1>
-      <section style={{ height: "30rem" }}>
-        <Dialog>
-          <p>Test</p>
-        </Dialog>
+const Docs: NextPageWithLayout = () => {
+  const [dialogVisible, setDialogVisible] = useState(true)
+
+  return (
+    <>
+      <section data-header="true">
+        <img src={ "/background.jpg" } alt=""/>
       </section>
-    </main>
-  </>
-);
+      <main>
+        <h1>Dialogs</h1>
+        <section style={ { height: "30rem" } }>
+          {dialogVisible && (
+          <Dialog onClose={ () => { console.log("dialog closed"); setDialogVisible(false) } }>
+            <p>Test</p>
+          </Dialog>
+        )}
+        </section>
+      </main>
+    </>
+  )
+}
 
 export default Docs;
 
-Docs.getLayout = page => (
+Docs.getLayout = page => {return (
   <HomeLayout noFooter>
     <DocsLayout>{page}</DocsLayout>
   </HomeLayout>
-)
+)}
