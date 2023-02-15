d6d4673471e51a70f8547fcf06b66df64a3eefbc
ewsgit <ewsgit@duck.com>
2023-02-15 20:32:54 +0000

    feat: display user's name in the welcome message for flow

--- projects/web/pages/app/flow/index.tsx
+++ projects/web/pages/app/flow/index.tsx
@@ -3,60 +3,77 @@ import AppLayout from '../../../layouts/appLayout/AppLayout';
 import { NextPageWithLayout } from '../../page';
 import styles from "./index.module.scss"
 import { useEffect, useState } from "react";
+import SERVER, { verifyAndReturnJson } from "../../../server";
+import { type CurrentUser } from "types/userManagement/currentUser"
+import Head from "next/head";
 
 const TasksIndex: NextPageWithLayout = () => {
   const [ fullName, setFullName ] = useState("")
 
   useEffect(() => {
-    setFullName()
+    verifyAndReturnJson(
+        SERVER.get(`/userManagement/current/user`),
+        (data: CurrentUser) => {
+          setFullName(`${data.name.first} ${data.name.last}`)
+        },
+        err => {
+          console.error(err)
+        }
+    )
+    setFullName("")
   }, [])
 
   return (
-    <div className={ styles.root }>
-      <Chiplet.Column className={ styles.sidebar }>
-        <h1>YourDash Flow</h1>
-        <div className={ styles.sidebarCollapsibleButtons }>
+    <>
+      <Head>
+        <title>YourDash | Flow</title>
+      </Head>
+      <div className={ styles.root }>
+        <Chiplet.Column className={ styles.sidebar }>
+          <h1>YourDash Flow</h1>
+          <div className={ styles.sidebarCollapsibleButtons }>
+            <Chiplet.Button>
+              Create
+            </Chiplet.Button>
+            <Chiplet.Button>
+              Create from template
+            </Chiplet.Button>
+          </div>
           <Chiplet.Button>
-            Create
+            Open File
           </Chiplet.Button>
-          <Chiplet.Button>
-            Create from template
-          </Chiplet.Button>
-        </div>
-        <Chiplet.Button>
-          Open File
-        </Chiplet.Button>
-      </Chiplet.Column>
-      <section className={ styles.main }>
-        <h1>Welcome back, </h1>
-        <Chiplet.Column className={ styles.recentProjects }>
-          <Chiplet.Card onClick={ () => {
-              return 0
-            } }
-          >
-            <span>Project 0</span>
-          </Chiplet.Card>
-          <Chiplet.Card onClick={ () => {
-              return 0
-            } }
-          >
-            <span>Project 1</span>
-          </Chiplet.Card>
-          <Chiplet.Card onClick={ () => {
-              return 0
-            } }
-          >
-            <span>Project 2</span>
-          </Chiplet.Card>
-          <Chiplet.Card onClick={ () => {
-              return 0
-            } }
-          >
-            <span>Project 3</span>
-          </Chiplet.Card>
         </Chiplet.Column>
-      </section>
-    </div>
+        <section className={ styles.main }>
+          <h1>Welcome back, {fullName}</h1>
+          <Chiplet.Column className={ styles.recentProjects }>
+            <Chiplet.Card onClick={ () => {
+                return 0
+              } }
+            >
+              <span>Project 0</span>
+            </Chiplet.Card>
+            <Chiplet.Card onClick={ () => {
+                return 0
+              } }
+            >
+              <span>Project 1</span>
+            </Chiplet.Card>
+            <Chiplet.Card onClick={ () => {
+                return 0
+              } }
+            >
+              <span>Project 2</span>
+            </Chiplet.Card>
+            <Chiplet.Card onClick={ () => {
+                return 0
+              } }
+            >
+              <span>Project 3</span>
+            </Chiplet.Card>
+          </Chiplet.Column>
+        </section>
+      </div>
+    </>
   )
 
 };
