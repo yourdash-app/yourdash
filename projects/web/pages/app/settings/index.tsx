0d8bbb4d076eb20cccaef35728a0ebf341f88f04
Ewsgit <ewsgit@duck.com>
2023-03-04 09:27:22 +0000

    style: formatting

--- projects/web/pages/app/settings/index.tsx
+++ projects/web/pages/app/settings/index.tsx
@@ -1,63 +1,61 @@
-import { useRouter } from 'next/router';
-import AppLayout from '../../../layouts/appLayout/AppLayout';
-import { NextPageWithLayout } from '../../page';
-import SettingsLayout from './components/SettingsLayout';
-import styles from "./index.module.scss"
+import { useRouter } from "next/router";
+import AppLayout from "../../../layouts/appLayout/AppLayout";
+import { NextPageWithLayout } from "../../page";
+import SettingsLayout from "./components/SettingsLayout";
+import styles from "./index.module.scss";
 import Chiplet from "ui";
 
 const SettingsIndex: NextPageWithLayout = () => {
-  const router = useRouter()
-  return (
-    <Chiplet.Column className={ styles.root }>
-      <div className={ styles.hero }>
-        <h2>Settings</h2>
-      </div>
-      <Chiplet.Card
-        className={ styles.card }
-        onClick={
-              () => {
-                router.push(`/app/settings/user/profile`)
-              }
-            }
-      >
-        Profile
-      </Chiplet.Card>
-      <Chiplet.Card
-        className={ styles.card }
-        onClick={ () => {
-              router.push(`/app/settings/user/panel`)
-            } }
-      >
-        Panel
-      </Chiplet.Card>
-      <Chiplet.Card
-        className={ styles.card }
-        onClick={ () => {
-              router.push(`/app/settings/user/notifications`)
-            } }
-      >
-        Notifications
-      </Chiplet.Card>
-      <Chiplet.Card
-        className={ styles.card }
-        onClick={ () => {
-              router.push(`/app/settings/user/theme`)
-            } }
-      >
-        Personal Theme
-      </Chiplet.Card>
-    </Chiplet.Column>
-  );
+    const router = useRouter();
+    return (
+        <Chiplet.Column className={styles.root}>
+            <div className={styles.hero}>
+                <h2>Settings</h2>
+            </div>
+            <main className={styles.grid}>
+                <Chiplet.Card
+                    className={styles.card}
+                    onClick={() => {
+                        router.push(`/app/settings/user/profile`);
+                    }}
+                >
+                    Profile
+                </Chiplet.Card>
+                <Chiplet.Card
+                    className={styles.card}
+                    onClick={() => {
+                        router.push(`/app/settings/user/panel`);
+                    }}
+                >
+                    Panel
+                </Chiplet.Card>
+                <Chiplet.Card
+                    className={styles.card}
+                    onClick={() => {
+                        router.push(`/app/settings/user/notifications`);
+                    }}
+                >
+                    Notifications
+                </Chiplet.Card>
+                <Chiplet.Card
+                    className={styles.card}
+                    onClick={() => {
+                        router.push(`/app/settings/user/theme`);
+                    }}
+                >
+                    Personal Theme
+                </Chiplet.Card>
+            </main>
+        </Chiplet.Column>
+    );
 };
 
 export default SettingsIndex;
 
-SettingsIndex.getLayout = page => {
-  return (
-    <AppLayout>
-      <SettingsLayout>
-        {page}
-      </SettingsLayout>
-    </AppLayout>
-  )
-}
+SettingsIndex.getLayout = (page) => {
+    return (
+        <AppLayout>
+            <SettingsLayout>{page}</SettingsLayout>
+        </AppLayout>
+    );
+};
