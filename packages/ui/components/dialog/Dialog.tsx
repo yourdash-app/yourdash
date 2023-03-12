926b78c82af0f1444b4dbc6a07c6c9e618db8c58
ewsgit <ewsgit@duck.com>
2023-03-12 13:33:27 +0000

    general: general changes

--- packages/ui/components/dialog/Dialog.tsx
+++ packages/ui/components/dialog/Dialog.tsx
@@ -1,26 +1,27 @@
 import React, { CSSProperties } from "react";
-import styles from "./Dialog.module.scss"
+import styles from "./Dialog.module.scss";
 import IconButton from "../iconButton/IconButton";
 
 export interface IDialog {
-  onClose?: () => void;
-  className?: string;
-  visible: boolean
-  style?: CSSProperties;
+    onClose?: () => void;
+    className?: string;
+    visible: boolean;
+    style?: CSSProperties;
 }
 
 const Dialog: React.FC<IDialog> = ({ children, onClose, className, visible, style }) => {
-  return (
-    <div className={ `${styles.component} ${!visible && styles.hidden}` } style={ style }>
-      <section className={ styles.handle }>
-        <div/>
-      </section>
-      <IconButton className={ styles.closeButton } data-visible={ !!onClose } icon={ "x-16" } onClick={ onClose }/>
-      <section className={ `${styles.content} ${className}` }>
-        {children}
-      </section>
-    </div>
-  )
+    return (
+        <div>
+            <button className={`${styles.background} ${!visible && styles.hidden}`} />
+            <div className={`${styles.component} ${!visible && styles.hidden}`} style={style}>
+                <section className={styles.handle}>
+                    <div />
+                </section>
+                <IconButton className={styles.closeButton} data-visible={!!onClose} icon={"x-16"} onClick={onClose} />
+                <section className={`${styles.content} ${className}`}>{children}</section>
+            </div>
+        </div>
+    );
 }
 
 export default Dialog
