57d7c7825fcb2351bec81222fef0049f1260d67b
ewsgit <ewsgit@duck.com>
2023-08-14 17:54:39 +0100

    chore: move application's directory

--- packages/web-client/src/app/apps/code_studio/ui/Symbols/Symbols.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Symbols: React.FC = () => (
-  <section className={"border-l border-l-container-border bg-container-bg h-full p-2"}>
-    <div>{"Symbols"}</div>
-  </section>
-);
-
-export default Symbols;
