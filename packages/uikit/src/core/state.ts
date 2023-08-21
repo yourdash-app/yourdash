085b8ef6590d79960376d009882c189d6e91e424
ewsgit <ewsgit@duck.com>
2023-08-21 21:26:14 +0100

    feat: create a working button component in UIKIT

---
+++ packages/uikit/src/core/state.ts
@@ -0,0 +1,23 @@
+export default class State<T> {
+  private value: T;
+  private hooks: ((value: T) => void)[]
+
+  constructor(initialValue: T) {
+    this.value = initialValue
+    this.hooks = []
+
+    return this
+  }
+
+  set(value: T): void {
+    this.value = value
+
+    this.hooks.forEach(hook => {
+      hook(this.value)
+    })
+  }
+
+  get(): T {
+    return this.value
+  }
+}
