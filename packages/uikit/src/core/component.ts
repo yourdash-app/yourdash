b3f5a72499268fa2be350545e127a040c1326c0b
ewsgit <ewsgit@duck.com>
2023-08-22 19:32:15 +0100

    work-in-progress: general changes to uikit

--- packages/uikit/src/core/component.ts
+++ packages/uikit/src/core/component.ts
@@ -6,15 +6,15 @@ export default class UKComponent<T extends UKComponentProps> {
   parentDomElement?: HTMLElement
   domElement: HTMLElement;
   props: T
-  private _hasRendered: boolean = false
+  private _hasRendered = false
 
-  constructor(props: T) {
+  constructor( props: T ) {
     this.props = props
-    this.domElement = document.createElement("div")
+    this.domElement = document.createElement( "div" )
   }
 
   _initialRender(): void {
-    if (this._hasRendered) console.warn(`WARNING: This component has already been initialized!`, this)
+    if ( this._hasRendered ) console.warn( "WARNING: This component has already been initialized!", this )
     this._hasRendered = true
     return
   }
--- packages/uikit/src/demo/main.ts
+++ packages/uikit/src/demo/main.ts
@@ -1,14 +1,44 @@
 import UIKit from "../core";
 import Button from "../core/components/buttons/button.ts";
+import Image from "../core/components/image/image.ts";
 
 function init() {
-  const ui = new UIKit(document.body)
+  const ui = new UIKit( document.body )
 
-  ui.add(Button, { label: "Button Label" })
-  ui.add(Button, { label: "Button Label" })
-  ui.add(Button, { label: "Button Label" })
-  ui.add(Button, { label: "Button Label" })
-  ui.add(Button, { label: "Button Label" })
+  ui.add( Button, {
+    label: "Button 1 Label",
+    onClick() {
+      console.log( "Button 1 Clicked" )
+    }
+  } )
+  ui.add( Button, {
+    label: "Button 2 Label",
+    onClick() {
+      console.log( "Button 2 Clicked" )
+    }
+  } )
+  ui.add( Button, {
+    label: "Button 3 Label",
+    onClick() {
+      console.log( "Button 3 Clicked" )
+    }
+  } )
+  ui.add( Button, {
+    label: "Button 4 Label",
+    onClick() {
+      console.log( "Button 4 Clicked" )
+    }
+  } )
+  ui.add( Button, {
+    label: "Button 5 Label",
+    onClick() {
+      console.log( "Button 5 Clicked" )
+    }
+  } )
+  ui.add( Image, {
+    src: "/vite.svg",
+    alt: "logo"
+  } )
 }
 
 init()
