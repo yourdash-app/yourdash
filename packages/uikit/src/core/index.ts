dd8550c26ed6eb799fe7b79b78ae4e7051948f58
ewsgit <ewsgit@duck.com>
2023-08-22 19:32:15 +0100

    work-in-progress: general changes to uikit

--- packages/uikit/src/core/index.ts
+++ packages/uikit/src/core/index.ts
@@ -1,5 +1,10 @@
-import UKComponent, { UKComponentProps } from "./component.ts";
+/**
+ * # UIKit - a vanilla JS UI library
+ * https://github.com/ewsgit/yourdash
+ */
 
+import UKComponent, { UKComponentProps } from "./component.ts";
+import defaultStyles from "./default.module.scss"
 export {}
 
 // @ts-ignore
@@ -8,24 +13,36 @@ window.__uikit__ = {
 }
 
 export default class UIKit {
-  domElement: HTMLDivElement = document.createElement("div")
+  domElement: HTMLDivElement = document.createElement( "div" )
   children: UKComponent<UKComponentProps>[] = []
 
-  constructor(container: HTMLElement) {
+  constructor( container: HTMLElement ) {
     // @ts-ignore
-    window.__uikit__.componentTree.push(this)
-    container.appendChild(this.domElement)
-
+    window.__uikit__.componentTree.push( this )
+    container.appendChild( this.domElement )
+    this.domElement.classList.add( defaultStyles.uikit )
+    
+    // define the accent color
+    this.domElement.style.setProperty( "--accent-bg", "#469ff5" )
+    
+    // define the accent color text color either black or white depending on the accent color's contrast
+    this.domElement.style.setProperty(
+      "--accent-fg",
+      window.getComputedStyle( document.body ).getPropertyValue( "--accent-color" ) === "#469ff5"
+        ? "0, 0, 0"
+        : "255, 255, 255"
+    )
+    
     return this
   }
 
-  add<T extends UKComponentProps>(component: typeof UKComponent<T>, props: UKComponent<T>["props"]) {
-    let comp = new component(props)
+  add<T extends UKComponentProps>( component: typeof UKComponent<T>, props: UKComponent<T>["props"] ) {
+    const comp = new component( props )
 
     comp.parentDomElement = this.domElement
-    comp.parentDomElement.appendChild(comp.domElement)
+    comp.parentDomElement.appendChild( comp.domElement )
 
-    this.children.push(comp)
+    this.children.push( comp )
     return comp
   }
 }
