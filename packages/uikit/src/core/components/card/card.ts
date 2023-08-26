ee8618379abb83f691de71b81d2d3bbf0b1aa8db
ewsgit <ewsgit@duck.com>
2023-08-26 10:50:28 +0100

    work-in-progress: general changes to uikit

--- packages/uikit/src/core/components/card/card.ts
+++ packages/uikit/src/core/components/card/card.ts
@@ -15,44 +15,36 @@ export interface CardProps extends UKComponentProps {
 }
 
 export default class Card extends UKComponent {
-  labelDomElement: HTMLSpanElement;
-  
   constructor( props: CardProps ) {
     super( props );
     
-    if ( !this.slots ) {
+    if ( !this.props.slots ) {
       // @ts-ignore
-      this.slots = {};
+      this.props.slots = {};
     }
     
-    this.domElement = document.createElement( "div" );
-    
-    const headerDomElement = document.createElement( "div" );
-    this.domElement.appendChild( headerDomElement );
-    
-    this.labelDomElement = document.createElement( "span" );
-    this.labelDomElement.innerText = this.props.label || "";
-    headerDomElement.appendChild( this.labelDomElement );
-    
-    const headerExtrasDomElement = document.createElement( "div" );
-    this.slots.headerExtras = new Slot( undefined, headerExtrasDomElement );
-    headerDomElement.appendChild( headerExtrasDomElement );
-    
-    const contentDomElement = document.createElement( "div" );
-    this.slots.content = new Slot( undefined, contentDomElement );
-    this.domElement.appendChild( contentDomElement );
-    
-    const contentHeaderDomElement = document.createElement( "div" );
-    this.slots.contentHeader = new Slot( undefined, contentHeaderDomElement );
-    contentDomElement.appendChild( contentHeaderDomElement );
-    
-    const contentFooterDomElement = document.createElement( "div" );
-    this.slots.contentFooter = new Slot( undefined, contentFooterDomElement );
-    contentDomElement.appendChild( contentFooterDomElement );
-    
-    const actionsDomElement = document.createElement( "div" );
-    this.slots.actions = new Slot( undefined, actionsDomElement );
-    contentDomElement.appendChild( actionsDomElement );
+    // this.domElement = document.createElement( "div" );
+    //
+    // const headerDomElement = document.createElement( "div" );
+    // this.domElement.appendChild( headerDomElement );
+    // this.labelDomElement = document.createElement( "span" );
+    // this.labelDomElement.innerText = this.props.label || "";
+    // headerDomElement.appendChild( this.labelDomElement );
+    // const headerExtrasDomElement = document.createElement( "div" );
+    // this.slots.headerExtras = new Slot( undefined, headerExtrasDomElement );
+    // headerDomElement.appendChild( headerExtrasDomElement );
+    // const contentDomElement = document.createElement( "div" );
+    // this.slots.content = new Slot( undefined, contentDomElement );
+    // this.domElement.appendChild( contentDomElement );
+    // const contentHeaderDomElement = document.createElement( "div" );
+    // this.slots.contentHeader = new Slot( undefined, contentHeaderDomElement );
+    // contentDomElement.appendChild( contentHeaderDomElement );
+    // const contentFooterDomElement = document.createElement( "div" );
+    // this.slots.contentFooter = new Slot( undefined, contentFooterDomElement );
+    // contentDomElement.appendChild( contentFooterDomElement );
+    // const actionsDomElement = document.createElement( "div" );
+    // this.slots.actions = new Slot( undefined, actionsDomElement );
+    // contentDomElement.appendChild( actionsDomElement );
     
     this.domElement.classList.add( styles.component );
     this.domElement.addEventListener( "click", this.click.bind( this ) );
