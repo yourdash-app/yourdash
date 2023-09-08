3819a18ee3816e14ce89a182c12d6b31c466a58f
ewsgit <ewsgit@duck.com>
2023-09-08 14:36:30 +0100

    Remove InstallationPopup.module.scss file and refactor other code
    Removed `InstallationPopup.module.scss` file as it's no longer needed and its code got moved to the relevant files.
    Concurrently, multiple adjustments and refactorings got executed, enhancing code quality and readability.
    Increased code parsing threshold on `editor.ts` to 256 to allow more lines to be parsed in a single run.
    Moreover, modified ESLin configurations on `.eslintrc` for better linting checks and refactored `LocationPage.tsx` to use hooks for navigations.
    License details got added to `vite.config.ts` as they were previously missing.
    `InstallationPopup.tsx` got largely reorganised to split large component tree with more manageable bits and reused many of existing components.
    Several other components received smaller modifications and refactorings including `page.tsx`, `ServerLoginPage.tsx`, `javascript.ts` to improve code organisation and maintainability.

--- packages/applications/store/frontend/application/components/InstallationPopup.tsx
+++ packages/applications/store/frontend/application/components/InstallationPopup.tsx
@@ -22,80 +22,91 @@
  */
 
 import * as React from "react";
-import { type IYourDashStoreApplication } from "shared/apps/store/storeApplication";
-import styles from "./InstallationPopup.module.scss";
-import { Card, IconButton, Dialog, MajorButton, Button } from "web-client/src/ui";
-import { YourDashIcon } from "web-client/src/ui/components/icon/iconDictionary";
 import { useNavigate } from "react-router-dom";
+import { type IYourDashStoreApplication } from "shared/apps/store/storeApplication";
+import { Button, Card, Dialog, IconButton, MajorButton } from "web-client/src/ui";
+import { YourDashIcon } from "web-client/src/ui/components/icon/iconDictionary";
 
 export interface IInstallationPopup {
   applicationData: IYourDashStoreApplication | undefined,
+  
   onClose(): void
+  
   onConfirm(): void
 }
 
-const InstallationPopup: React.FC<IInstallationPopup> = ( { applicationData, onClose, onConfirm } ) => {
+const InstallationPopup: React.FC<IInstallationPopup> = ( {
+  applicationData,
+  onClose,
+  onConfirm
+} ) => {
   const navigate = useNavigate();
-  return (
-    <Dialog onClose={() => {
-      onClose();
-    }}
+  return ( <Dialog onClose={ () => {
+    onClose();
+  } }
+  >
+    <Card
+      showBorder
+      level={ "secondary" }
     >
-      <Card showBorder className={styles.component}>
-        <section className={"flex items-center justify-between"}>
-          <h1 className={"text-3xl font-semibold tracking-wide"}>{`Confirm installation of ${ applicationData?.displayName }`}</h1>
-          <IconButton
-            icon={YourDashIcon.X}
-            onClick={() => {
-              onClose();
-            }}
-          />
-        </section>
-        <section className={"flex flex-col items-center gap-2"}>
-          <div>
-            {`Do you want to install ${ applicationData?.displayName }?`}
-          </div>
-          <Card level={"secondary"} className={"flex flex-col gap-2"}>
+      <section
+        className={ "flex items-center justify-between" }
+      >
+        <h1 className={ "text-3xl font-semibold tracking-wide" }>{ `Confirm installation of ${ applicationData?.displayName }` }</h1>
+        <IconButton
+          icon={ YourDashIcon.X }
+          onClick={ () => {
+            onClose();
+          } }
+        />
+      </section>
+      <section className={ "flex flex-col items-center gap-2" }>
+        <div>
+          { `Do you want to install ${ applicationData?.displayName }?` }
+        </div>
+        { ( applicationData?.dependencies && applicationData.dependencies?.length !== 0 ) && (
+          <Card
+            level={ "tertiary" }
+            className={ "flex flex-col gap-2" }
+          >
             <div>
-              {"The following dependency applications will be installed automatically."}
+              { "The following dependency applications will be installed automatically." }
             </div>
-            <div className={"flex flex-wrap w-full"}>
+            <div className={ "flex flex-wrap w-full" }>
               {
-                applicationData?.dependencies && (
-                  applicationData.dependencies.map( dependency => (
-                    <Card
-                      className={"p-2 flex-grow text-center"}
-                      level={"tertiary"}
-                      onClick={() => {
-                        navigate( `/app/a/store/app/${ dependency }` );
-                      }}
-                      key={dependency}
-                    >
-                      {dependency}
-                    </Card>
-                  ) )
-                )
+                applicationData.dependencies.map( dependency => (
+                  <Card
+                    className={ "p-2 flex-grow text-center" }
+                    level={ "tertiary" }
+                    onClick={() => {
+                      navigate( `/app/a/store/app/${ dependency }` );
+                    }}
+                    key={ dependency }
+                  >
+                    { dependency }
+                  </Card>
+                ) )
               }
             </div>
           </Card>
-          <div className={"flex gap-2"}>
-            <Button onClick={() => {
-              onClose();
-            }}
-            >
-              {"Cancel"}
-            </Button>
-            <MajorButton onClick={() => {
-              onConfirm();
-            }}
-            >
-              {"Confirm installation"}
-            </MajorButton>
-          </div>
-        </section>
-      </Card>
-    </Dialog>
-  );
+        )}
+        <div className={ "flex gap-2" }>
+          <Button onClick={ () => {
+            onClose();
+          } }
+          >
+            { "Cancel" }
+          </Button>
+          <MajorButton onClick={ () => {
+            onConfirm();
+          } }
+          >
+            { "Confirm installation" }
+          </MajorButton>
+        </div>
+      </section>
+    </Card>
+  </Dialog> );
 };
 
 export default InstallationPopup;
--- packages/web-client/src/ComingSoon.tsx
+++ packages/web-client/src/ComingSoon.tsx
@@ -1,22 +1,18 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const ComingSoon: React.FC = () => {
-  return (
-    <div
-      className={ "min-h-full w-full text-base-50 flex items-center justify-center text-5xl font-bold tracking-wide " +
-                "flex-col gap-4" }
+  return ( <div
+    className={ "min-h-full w-full text-base-50 flex items-center justify-center text-5xl font-bold tracking-wide flex-col gap-4" }
+  >
+    <span>Coming soon...</span>
+    <Link
+      to={ "/" }
+      className={ "pl-8 pr-8 pb-1.5 pt-1.5 hover:bg-theme-500 active:bg-theme-400 bg-theme-600 transition-colors select-none cursor-pointer rounded-full text-xl font-normal tracking-normal" }
     >
-      <span>Coming soon...</span>
-      <Link
-        to={ "/" }
-        className={ "pl-8 pr-8 pb-1.5 pt-1.5 hover:bg-theme-500 active:bg-theme-400 bg-theme-600 transition-colors " +
-                  "select-none cursor-pointer rounded-full text-xl font-normal tracking-normal" }
-      >
-        Home
-      </Link>
-    </div>
-  )
-}
+      Home
+    </Link>
+  </div> );
+};
 
-export default ComingSoon
+export default ComingSoon;
