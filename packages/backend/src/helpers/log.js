dc75ecc7eb0429451aacf4c256eb4070abe75fc3
ewsgit <ewsgit@duck.com>
2023-09-26 19:28:06 +0100

    chore: improve dev toolchain

--- packages/backend/src/helpers/log.js
+++ packages/backend/src/helpers/log.js
@@ -1,64 +1,59 @@
-/*
- * Copyright ©2023 @Ewsgit and YourDash contributors.
- * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
- */
-
 import chalk from "chalk";
 import globalDatabase from "./globalDatabase.js";
 import sourceMapSupport from "source-map-support";
 sourceMapSupport.install();
 export var logType;
-( function ( logType ) {
-  logType[logType["INFO"] = 0] = "INFO";
-  logType[logType["WARNING"] = 1] = "WARNING";
-  logType[logType["ERROR"] = 2] = "ERROR";
-  logType[logType["SUCCESS"] = 3] = "SUCCESS";
-} )( logType || ( logType = {} ) );
+(function (logType) {
+    logType[logType["INFO"] = 0] = "INFO";
+    logType[logType["WARNING"] = 1] = "WARNING";
+    logType[logType["ERROR"] = 2] = "ERROR";
+    logType[logType["SUCCESS"] = 3] = "SUCCESS";
+})(logType || (logType = {}));
 export const LOG_HISTORY = [];
-export default function log( type, ...message ) {
-  const logParams = [];
-  if ( globalDatabase.get( "settings:log_should_log_time" ) ) {
-    const date = new Date();
-    logParams.push( `${date.getHours()}:${date.getMinutes()}:${date.getSeconds() < 10
-      ? `${date.getSeconds()}0`
-      : date.getSeconds()} ` );
-  }
-  switch ( type ) {
-  case logType.INFO:
-    logParams.push( chalk.blue( "INFO    " ) );
-    break;
-  case logType.WARNING:
-    logParams.push( chalk.yellow( "WARN    " ) );
-    break;
-  case logType.ERROR:
-    logParams.push( chalk.red( "ERROR   " ) );
-    break;
-  case logType.SUCCESS:
-    logParams.push( chalk.green( "SUCCESS " ) );
-    break;
-  default:
-    break;
-  }
-  if ( type === logType.ERROR ) {
-    let stackTrace = new Error( message.toString() ).stack.slice( 7 );
-    const stackArray = stackTrace.split( "\n" );
-    stackArray.splice( 1, 1 );
-    stackTrace = stackArray.join( "\n" );
-    logParams.push( stackTrace );
-  }
-  else {
-    logParams.push( ...message );
-    LOG_HISTORY.push( {
-      type: type === logType.INFO
-        ? "INFO"
-        : type === logType.WARNING
-          ? "WARN"
-          : type === logType.SUCCESS
-            ? "SUCCESS"
-            : "UNKNOWN",
-      message: logParams.slice( 1 ).map( msg => msg?.replace?.( /\x1b\[[0-9;]*m/g, "" ) || "LOGGING ERROR" )
-    } );
-  }
-  console.log( ...logParams );
+export default function log(type, ...message) {
+    const logParams = [];
+    if (globalDatabase.get("settings:log_should_log_time")) {
+        const date = new Date();
+        logParams.push(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds() < 10
+            ? `${date.getSeconds()}0`
+            : date.getSeconds()} `);
+    }
+    switch (type) {
+        case logType.INFO:
+            logParams.push(chalk.blue("INFO    "));
+            break;
+        case logType.WARNING:
+            logParams.push(chalk.yellow("WARN    "));
+            break;
+        case logType.ERROR:
+            logParams.push(chalk.red("ERROR   "));
+            break;
+        case logType.SUCCESS:
+            logParams.push(chalk.green("SUCCESS "));
+            break;
+        default:
+            break;
+    }
+    if (type === logType.ERROR) {
+        let stackTrace = new Error(message.toString()).stack.slice(7);
+        const stackArray = stackTrace.split("\n");
+        stackArray.splice(1, 1);
+        stackTrace = stackArray.join("\n");
+        logParams.push(stackTrace);
+    }
+    else {
+        logParams.push(...message);
+        LOG_HISTORY.push({
+            type: type === logType.INFO
+                ? "INFO"
+                : type === logType.WARNING
+                    ? "WARN"
+                    : type === logType.SUCCESS
+                        ? "SUCCESS"
+                        : "UNKNOWN",
+            message: logParams.slice(1).map(msg => msg?.replace?.(/\x1b\[[0-9;]*m/g, "") || "LOGGING ERROR")
+        });
+    }
+    console.log(...logParams);
 }
-// # sourceMappingURL=log.js.map
+//# sourceMappingURL=log.js.map
