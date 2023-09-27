57c9a166c5012240c3968aa1f7ad2ae486bab31e
ewsgit <ewsgit@duck.com>
2023-09-27 21:29:47 +0100

    feat: create OpenGFX engine

---
+++ packages/opengfx-engine/index.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright ©2023 @Ewsgit and YourDash contributors.
+ * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
+ */
+
+import Engine from "./src/core/index.ts";
+
+export default Engine
