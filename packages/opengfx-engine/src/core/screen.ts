e1653b86ecd905502f7a979a997706eff6d96b3b
ewsgit <ewsgit@duck.com>
2023-09-28 19:39:06 +0100

    work-in-progress: add resizing of the window and begin to add object and scene support to OpenGFX engine

---
+++ packages/opengfx-engine/src/core/screen.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright ©2023 @Ewsgit and YourDash contributors.
+ * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
+ */
+
+export default class Screen {
+  width = 400;
+  height = 200;
+  aspectRatio = 2;
+  private screenContainer: HTMLElement;
+  private screenElement: HTMLCanvasElement;
+  
+  constructor( screenContainer: HTMLElement, screenElement: HTMLCanvasElement ) {
+    this.screenContainer = screenContainer;
+    this.screenElement = screenElement;
+    
+    this.screenElement.width = this.width;
+    this.screenElement.height = this.height;
+    
+    let timeout: NodeJS.Timeout;
+    
+    // on resize, with a debounce delay of 100ms, update the screen
+    window.addEventListener( "resize", () => {
+      if ( timeout ) {
+        clearTimeout( timeout );
+      }
+      timeout = setTimeout( this.update.bind( this ), 100 );
+    } );
+  }
+  
+  update() {
+    this.width = this.screenContainer.offsetWidth;
+    this.height = this.screenContainer.offsetHeight;
+    this.aspectRatio = this.width / this.height;
+    
+    console.debug( `GFX screen updated X:${ this.width } Y:${ this.height }` );
+    
+    this.screenElement.width = this.width;
+    this.screenElement.height = this.height;
+  }
+}
