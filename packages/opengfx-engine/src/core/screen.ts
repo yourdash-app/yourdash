8cee7e5e8d11095abfe85d083f79952a16b2d391
ewsgit <ewsgit@duck.com>
2023-09-30 12:22:10 +0100

    feat: begin rendering objects

--- packages/opengfx-engine/src/core/screen.ts
+++ packages/opengfx-engine/src/core/screen.ts
@@ -3,19 +3,38 @@
  * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
  */
 
-export default class Screen {
+import { GFX_RENDER_PIPELINE } from "./render/pipelines/index.ts";
+
+export default class Screen<T extends GFX_RENDER_PIPELINE> {
   width = 400;
   height = 200;
   aspectRatio = 2;
   private screenContainer: HTMLElement;
   private screenElement: HTMLCanvasElement;
+  context: ( T extends GFX_RENDER_PIPELINE.TwoDimensional ? CanvasRenderingContext2D : WebGL2RenderingContext ) | null;
   
-  constructor( screenContainer: HTMLElement, screenElement: HTMLCanvasElement ) {
+  constructor( screenContainer: HTMLElement, screenElement: HTMLCanvasElement, pipeline: T ) {
     this.screenContainer = screenContainer;
     this.screenElement = screenElement;
     
     this.screenElement.width = this.width;
     this.screenElement.height = this.height;
+    
+    switch ( pipeline ) {
+    case GFX_RENDER_PIPELINE.TwoDimensional:
+      // @ts-ignore
+      this.context = this.screenElement.getContext( "2d" );
+      break;
+    case GFX_RENDER_PIPELINE.ThreeDimensional:
+      // @ts-ignore
+      this.context = this.screenElement.getContext( "webgl2" );
+      break;
+    default:
+      // @ts-ignore
+      this.context = this.screenElement.getContext( "2d" );
+      console.error( "Invalid render pipeline supplied to screen" );
+      break;
+    }
     
     let timeout: NodeJS.Timeout;
     
--- packages/opengfx-engine/src/demo/2d/gfxts-one/src/main.ts
+++ packages/opengfx-engine/src/demo/2d/gfxts-one/src/main.ts
@@ -4,7 +4,14 @@
  */
 
 import Engine from "opengfx"
+import GFXBox from "../../../../core/object/2d/box.ts";
+import Scene from "../../../../core/scene/scene"
 
-const engine = new Engine()
+const engine = new Engine( document.body as HTMLDivElement )
 
-engine.start( document.body as HTMLDivElement )
+const box = new GFXBox()
+
+engine.setScene( new Scene( { id: "default_scene", objects: [box] } ) )
+
+box.setPosition( 50, 50 )
+box.render( engine.screen )
