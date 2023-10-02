93c28b0970f5a01a62be11c019399976feeb8a9d
ewsgit <ewsgit@duck.com>
2023-10-02 21:22:35 +0100

    general: OpenGFX: render triangle

--- packages/opengfx-engine/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright ©2023 @Ewsgit and YourDash contributors.
- * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
- */
-
-import Engine from "./src/core/index.ts";
-
-export default Engine
