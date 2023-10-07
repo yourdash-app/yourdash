4f2a1eaad51e4ac33f30d580fac23cab3e951e7a
ewsgit <ewsgit@duck.com>
2023-10-07 13:23:50 +0100

    general: fix backend manager

--- packages/shared/core/session.js
+++ packages/shared/core/session.js
@@ -1,3 +1,7 @@
+/*
+ * Copyright ©2023 @Ewsgit and YourDash contributors.
+ * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
+ */
 export var YOURDASH_SESSION_TYPE;
 (function (YOURDASH_SESSION_TYPE) {
     YOURDASH_SESSION_TYPE[YOURDASH_SESSION_TYPE["web"] = 0] = "web";
@@ -5,4 +9,3 @@ export var YOURDASH_SESSION_TYPE;
     YOURDASH_SESSION_TYPE[YOURDASH_SESSION_TYPE["cli"] = 2] = "cli";
     YOURDASH_SESSION_TYPE[YOURDASH_SESSION_TYPE["external"] = 3] = "external";
 })(YOURDASH_SESSION_TYPE || (YOURDASH_SESSION_TYPE = {}));
-//# sourceMappingURL=session.js.map
