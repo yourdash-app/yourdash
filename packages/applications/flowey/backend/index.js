6836d213c43e996e2c3b9239328ff4932d376bbb
ewsgit <ewsgit@duck.com>
2023-10-15 15:21:13 +0100

    chore: removed backend-compiled files from git

--- packages/applications/flowey/backend/index.js
+++
@@ -1,7 +0,0 @@
-import YourDashModule from "backend/src/core/yourDashModule.js";
-export default class FloweyModule extends YourDashModule {
-    constructor(args) {
-        super(args);
-    }
-}
-//# sourceMappingURL=index.js.map
