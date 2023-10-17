68cefaaccd275bfa09ac2f16ac1c1ab975cd4f38
Ewsgit <ewsgit@duck.com>
2023-10-17 19:29:28 +0100

    general: general changes

--- packages/uikit/src/demo/sections/buttons/disabledButtons.ts
+++ packages/uikit/src/demo/sections/buttons/disabledButtons.ts
@@ -3,32 +3,32 @@
  * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
  */
 
-import Button from "../../../core/components/buttons/button/button.ts";
+import Button from "../../../core/components/button/button.ts";
 import Card from "../../../core/components/card/card.ts";
 
-export default function createDisabledButtons(card: Card) {
-  card.slots.content.createComponent(Button, {
+export default function createDisabledButtons( card: Card ) {
+  card.slots.content.createComponent( Button, {
     label: "Button 1 Label",
     onClick() {
-      console.log("Button 1 Clicked")
+      console.log( "Button 1 Clicked" )
     },
     size: "small",
     disabled: true
-  })
-  card.slots.content.createComponent(Button, {
+  } )
+  card.slots.content.createComponent( Button, {
     label: "Button 2 Label",
     onClick() {
-      console.log("Button 2 Clicked")
+      console.log( "Button 2 Clicked" )
     },
     size: "medium",
     disabled: true
-  })
-  card.slots.content.createComponent(Button, {
+  } )
+  card.slots.content.createComponent( Button, {
     label: "Button 3 Label",
     onClick() {
-      console.log("Button 3 Clicked")
+      console.log( "Button 3 Clicked" )
     },
     size: "large",
     disabled: true
-  })
+  } )
 }
