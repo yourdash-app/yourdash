1d8fd620dfe9ededef3a3776b205d846149014ba
ewsgit <ewsgit@duck.com>
2023-10-18 21:50:25 +0100

    general: OpenGFX engine changes

--- packages/opengfx-engine/src/demo/2d/gfxts-one/src/main.ts
+++ packages/opengfx-engine/src/demo/2d/gfxts-one/src/main.ts
@@ -4,11 +4,11 @@
  */
 
 import initEngine from "opengfx"
-import loadObject from "../../../../core/object/loadObject.ts";
+import loadObject from "../../../../runtime/core/object/loadObject.ts";
 
 // @ts-ignore
 const engine = await initEngine( document.body as HTMLDivElement )
 
-await loadObject( "/boeing/boeing_787.obj?raw", engine.screen, engine )
+await loadObject( new URL( "/boeing/boeing_787.obj", import.meta.url ).toString(), engine.screen, engine )
 
 // engine.setScene( new Scene( { id: "default_scene", objects: [] }, engine.screen ) )
