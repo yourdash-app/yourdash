1394a0b349f6230469b136090458c60e35454ee2
ewsgit <ewsgit@duck.com>
2023-11-13 21:33:53 +0000

    working: misc working changes 13/11/23

--- packages/desktop-client/src/preload/index.d.ts
+++ packages/desktop-client/src/preload/index.d.ts
@@ -1,8 +1,7 @@
-import { ElectronAPI } from '@electron-toolkit/preload'
+import { ElectronAPI } from "@electron-toolkit/preload";
 
 declare global {
   interface Window {
-    electron: ElectronAPI
-    api: unknown
+    electron: ElectronAPI;
   }
 }
