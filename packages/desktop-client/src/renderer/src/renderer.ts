06f5691ed4d062b5c004c57df4746d0242631b65
ewsgit <ewsgit@duck.com>
2023-11-13 17:24:28 +0000

    working: misc working changes 13/11/23

--- packages/desktop-client/src/renderer/src/renderer.ts
+++ packages/desktop-client/src/renderer/src/renderer.ts
@@ -1,15 +1,9 @@
+const ELECTRON = window.electron
+
 export function init(): void {
   window.addEventListener('DOMContentLoaded', () => {
     doAThing()
   })
-}
-
-function doAThing(): void {
-  const versions = window.electron.process.versions
-  replaceText('.electron-version', `Electron v${versions.electron}`)
-  replaceText('.chrome-version', `Chromium v${versions.chrome}`)
-  replaceText('.node-version', `Node v${versions.node}`)
-  replaceText('.v8-version', `V8 v${versions.v8}`)
 }
 
 function replaceText(selector: string, text: string): void {
