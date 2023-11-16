a829a07cf47836509d6ac64357f10ec5ada41ef9
Ewsgit <ewsgit@duck.com>
2023-11-16 19:35:22 +0000

    fix: fix the build and development installation for the YourDash Desktop Client

--- packages/desktop-client/electron.vite.config.ts
+++ packages/desktop-client/electron.vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 
-export default defineConfig({
+export default defineConfig( {
   main: {
     plugins: [
       externalizeDepsPlugin()
-    ]
+    ],
   },
   preload: {
     plugins: [
       externalizeDepsPlugin()
     ]
   },
-  renderer: {}
-})
+  renderer: {
+    server: {
+      port: 5174
+    }
+  }
+} )
