210ae9397c6a4037a9ce8788ee47825ad6090c43
Ewsgit <ewsgit@duck.com>
2023-11-25 13:09:08 +0000

    working: general wip changes 25/11/23

--- packages/backend/src/core/core/personalServerAccelerator/personalServerAcceleratorConnection.ts
+++ packages/backend/src/core/core/personalServerAccelerator/personalServerAcceleratorConnection.ts
@@ -3,46 +3,50 @@
  * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
  */
 
-import { Socket as SocketIoSocket } from "socket.io"
+import { Socket as SocketIoSocket } from "socket.io";
 import { IYourDashSession, YOURDASH_SESSION_TYPE } from "../user/session.js";
-import CoreApiWebsocketManager from "./coreApiWebsocketManager.js";
+import CoreApiPersonalServerAccelerator from "./coreApiPersonalServerAccelerator.js";
 
-export default class WebsocketConnection {
+export default class PersonalServerAcceleratorConnection {
   username: string;
   session: IYourDashSession<YOURDASH_SESSION_TYPE.desktop>;
   private socket: SocketIoSocket | undefined;
-  private websocketManager: CoreApiWebsocketManager;
+  private websocketManager: CoreApiPersonalServerAccelerator;
   id: `${string}-${number}` /* `[username]-[sessionId]` */;
-  
+
   constructor(
     username: string,
     session: IYourDashSession<YOURDASH_SESSION_TYPE.desktop>,
     socket: SocketIoSocket,
-    websocketManager: CoreApiWebsocketManager
+    websocketManager: CoreApiPersonalServerAccelerator
   ) {
     this.username = username;
     this.session = session;
     this.socket = socket;
     this.websocketManager = websocketManager;
-    
+
     return this
   }
-  
+
   close() {
     this.socket.disconnect()
-    
+
     this.websocketManager.__internal__removeSocketConnection( this )
   }
-  
+
   listenFor( path: string, callBack: ( data: any ) => void ) {
     this.socket.on( path, callBack );
-    
+
     return this
   }
-  
+
   emit( path: string, data: any ) { // eslint-disable-line @typescript-eslint/no-explicit-any
     this.socket.emit( path, data );
 
     return this
+  }
+
+  executeCommand( command: string ) {
+    this.emit( "core:execute_command", command )
   }
 }
