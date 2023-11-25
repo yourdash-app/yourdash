c82379cac153e47c13fc0d682e869156830fd1cd
Ewsgit <ewsgit@duck.com>
2023-11-25 10:13:22 +0000

    working: general wip changes 25/11/23

---
+++ packages/desktop-client/src/renderer/src/views/pushNotificationMenu/PushNotificationMenu.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright ©2023 @Ewsgit and YourDash contributors.
+ * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
+ */
+
+import * as UIKit from "@yourdash/uikit/src/core/index";
+import transitions from "@yourdash/uikit/src/core/transitions.module.scss";
+import styles from "./pushNotificationMenu.module.scss";
+
+export default class PushNotificationMenu extends UIKit.Box {
+  constructor() {
+    super( { flex: "column", alignItems: "center", justifyContent: "center" }  );
+
+    this.domElement.classList.add( styles.menu )
+    this.domElement.classList.add( transitions.UKTransition_appear )
+    this.setVisible( false )
+  }
+
+  setVisible( visible: boolean ) {
+    if ( visible ) {
+      this.domElement.style.display = "flex"
+      this.domElement.style.visibility = "visible"
+    } else {
+      this.domElement.style.display = "none"
+      this.domElement.style.visibility = "hidden"
+    }
+  }
+}
