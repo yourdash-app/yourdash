6f7c3115c630a76efc404657e029a03c71319279
Ewsgit <ewsgit@duck.com>
2023-11-28 18:05:01 +0000

    working: general wip changes 28/11/23

--- packages/web-client/src/app/panel/Panel.tsx
+++ packages/web-client/src/app/panel/Panel.tsx
@@ -15,35 +15,36 @@ const Panel: React.FC<{ side: "top" | "right" | "bottom" | "left", setLayoutRelo
   const [widgets, setWidgets] = useState<string[]>( ["InstanceLogo", "ApplicationLauncher", "Separator", "QuickShortcuts"] )
   const [panelSize, setPanelSize] = useState<"small" | "medium" | "large" | undefined>( undefined )
   const [num, setNum] = useState<number>( 0 )
-  
+
   useEffect( () => {
     setPanelSize( csi.userDB.get( "core:panel:size" ) || "medium" )
   }, [num] );
-  
+
   // @ts-ignore
   window.__yourdashCorePanelReload = () => {
     setNum( num + 1 )
     setLayoutReloadNumber( num + 1 )
   }
-  
+
   if ( panelSize === undefined ) {
     return <></>
   }
-  
+
   return <section className={clippy(
     styles.panel,
-    side === "top" && styles.top,
-    side === "right" && styles.right,
-    side === "bottom" && styles.bottom,
-    side === "left" && styles.left,
+    side === "top" && styles.top + " animate__fadeInDown",
+    side === "right" && styles.right + " animate__fadeInRight",
+    side === "bottom" && styles.bottom + " animate__fadeInUp",
+    side === "left" && styles.left + " animate__fadeInLeft",
     panelSize === "small" && styles.small,
     panelSize === "medium" && styles.medium,
-    panelSize === "large" && styles.large
+    panelSize === "large" && styles.large,
+    "animate__animated"
   )}>
     {
       widgets.map( widget => {
         const LoadableWidget = loadable( () => import( `./widgets/${widget}/Widget` ) )
-        
+
         return <LoadableWidget key={widget} side={side} />
       } )
     }
