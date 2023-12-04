f50878924690e66b1d338930874cc10a2438d2b6
ewsgit <ewsgit@duck.com>
2023-12-04 21:30:26 +0000

    working: general wip changes 04/12/23

--- packages/applications/code_studio/frontend/core/editor/editor.ts
+++ packages/applications/code_studio/frontend/core/editor/editor.ts
@@ -6,8 +6,9 @@
 import KeyboardInputManager from "./keyboardManager";
 import { CodeStudioFileTypes } from "./fileTypes";
 import TreeSitterParser from "web-tree-sitter"
+import styles from "./editor.module.scss"
 // @ts-ignore
-import JAVASCRIPT_PARSER from "./languages/javascript.wasm?url"
+import TYPESCRIPT_PARSER from "./languages/typescript.wasm?url"
 // @ts-ignore
 import TREESITTER_WASM from "./tree-sitter.wasm?url"
 
@@ -28,16 +29,185 @@ export default class CodeStudioEditor {
     } } )
     const parser = new TreeSitterParser();
 
-    console.log( JAVASCRIPT_PARSER )
+    console.log( TYPESCRIPT_PARSER )
 
-    parser.setLanguage( await TreeSitterParser.Language.load( JAVASCRIPT_PARSER ) );
+    parser.setLanguage( await TreeSitterParser.Language.load( TYPESCRIPT_PARSER ) );
 
-    return this._debugRenderParsedString( rawCode, parser );
+    return this.renderParsedString( parser.parse( rawCode ) );
   }
 
-  _debugRenderParsedString( string: string, parser: TreeSitterParser ) {
+  renderParsedString( parser: TreeSitterParser.Tree ) {
     this.htmlContainer.innerHTML = "<pre><code id=\"cs-text-output\"></code></pre>";
 
-    return parser.parse( string )
+    const codeElement = document.getElementById( "cs-text-output" ) as HTMLDivElement;
+    codeElement.classList.add( styles.editor )
+
+    const rendered_items: TreeSitterParser.SyntaxNode[] = []
+
+    function renderToken( parentElement: HTMLDivElement | HTMLSpanElement, syntaxNode: TreeSitterParser.SyntaxNode ) {
+      if ( rendered_items.indexOf( syntaxNode ) !== -1 ) {
+        console.error( "CIRCULAR REFERENCE" )
+      }
+      rendered_items.push( syntaxNode )
+
+
+      if ( syntaxNode.childCount > 0 ) {
+        const tokenElement = document.createElement( "span" )
+
+        parentElement.appendChild( tokenElement )
+
+        for ( let i = 0; i < syntaxNode.childCount; i++ ) {
+          renderToken( tokenElement, syntaxNode.child( i )! ) // eslint-disable-line @typescript-eslint/no-non-null-assertion
+        }
+
+        return
+      }
+
+      const tokenElement = document.createElement( "span" )
+
+      tokenElement.innerText = syntaxNode.text
+
+      switch ( syntaxNode.type ) {
+      case "type_identifier":
+      case "type_parameter":
+      case "type_parameters":
+        tokenElement.classList.add( styles.type_identifier )
+        break
+      case "comment":
+        tokenElement.classList.add( styles.comment )
+        break
+      case "string":
+      case "string_fragment":
+        tokenElement.classList.add( styles.string )
+        break
+      case "number":
+        tokenElement.classList.add( styles.number )
+        break
+      case "property_identifier":
+        tokenElement.classList.add( styles.propertyIdentifier )
+        break
+      case "identifier":
+        tokenElement.classList.add( styles.identifier )
+        break
+      case "operator":
+      case "+":
+      case "-":
+      case "=":
+      case "_":
+      case ">":
+      case "<":
+      case "<<":
+      case ">>":
+      case "--":
+      case "++":
+      case "&":
+      case "&&":
+      case "*":
+      case "^":
+      case "%":
+      case "$":
+      case "!":
+      case "!!":
+      case "|":
+      case "||":
+      case "`":
+      case "!=":
+      case "!==":
+      case "==":
+      case "===":
+      case "=>":
+      case ">=":
+      case "<=":
+      case "?":
+      case "...":
+        tokenElement.classList.add( styles.operator )
+        break
+      case "\"":
+      case ".":
+      case ",":
+      case "'":
+      case ";":
+      case ":":
+      case "}":
+      case "{":
+      case "[":
+      case "]":
+      case "(":
+      case ")":
+      case "${":
+      case "\\":
+      case "/":
+        tokenElement.classList.add( styles.punctuation )
+        break
+      case "regex_pattern":
+      case "regex_flags":
+      case "extends":
+      case "async":
+      case "abstract":
+      case "do":
+      case "keyword":
+      case "module":
+      case "export":
+      case "function":
+      case "var":
+      case "let":
+      case "const":
+      case "class":
+      case "interface":
+      case "type":
+      case "if":
+      case "else":
+      case "for":
+      case "while":
+      case "switch":
+      case "case":
+      case "default":
+      case "try":
+      case "catch":
+      case "finally":
+      case "return":
+      case "break":
+      case "continue":
+      case "new":
+      case "delete":
+      case "typeof":
+      case "instanceof":
+      case "void":
+      case "null":
+      case "true":
+      case "false":
+      case "this":
+      case "undefined":
+      case "in":
+      case "yield":
+      case "await":
+      case "from":
+      case "import":
+      case "import_statement":
+      case "export_statement":
+        tokenElement.classList.add( styles.keyword )
+        break
+      default:
+        console.log( syntaxNode.type )
+        const afterElement = document.createElement( "span" )
+        tokenElement.appendChild( afterElement )
+        afterElement.style.background = "red"
+        afterElement.style.color = "white"
+        afterElement.style.width = "max-content"
+        afterElement.innerText = syntaxNode.type
+        tokenElement.style.background = "linear-gradient(45deg, rgba(255,255,0,1) 0%, rgba(0,0,0,1) 50%, rgba(255,255,0,1) 100%)"
+        tokenElement.style.backgroundSize = "100% 100%"
+        tokenElement.style.backgroundRepeat = "repeat"
+        break
+      }
+
+      tokenElement.classList.add( styles.symbol )
+
+      parentElement.appendChild( tokenElement )
+    }
+
+    renderToken( codeElement, parser.rootNode )
+
+    return parser
   }
 }
--- packages/applications/files/frontend/views/preview/TextPreview.tsx
+++ packages/applications/files/frontend/views/preview/TextPreview.tsx
@@ -6,9 +6,8 @@
 import * as React from "react";
 import CodeStudioEditor from "applications/code_studio/frontend/core/editor/editor";
 import csi from "web-client/src/helpers/csi";
-import getParserForFileExtension from "applications/code_studio/frontend/core/editor/languageParser/parserLookup";
+import getParserForFileExtension from "applications/code_studio/frontend/core/editor/editor";
 import * as pathBrowserify from "path-browserify";
-import CodeStudioLanguageParser from "applications/code_studio/frontend/core/editor/languageParser/parser";
 import { IconButton } from "web-client/src/ui";
 import { YourDashIcon } from "web-client/src/ui/components/icon/iconDictionary";
 
@@ -18,38 +17,38 @@ export interface ITextPreview {
 
 const TextPreview: React.FC<ITextPreview> = ( { path = "" } ) => {
   const ref = React.useRef<HTMLDivElement>( null );
-  const [formatJson, setFormatJson] = React.useState( getParserForFileExtension( pathBrowserify.extname( path ).replace( ".", "" ) ) === "json" );
-  
+  const [ formatJson, setFormatJson ] = React.useState( getParserForFileExtension( pathBrowserify.extname( path ).replace( ".", "" ) ) === "json" );
+
   React.useEffect( () => {
     if ( !ref.current ) {
       return;
     }
-    
+
     const editor = new CodeStudioEditor( ref.current );
-    
+
     // @ts-ignore
     csi.postText( "/app/files/get/file", { path }, resp => {
       let content = resp;
-      
+
       if ( formatJson ) {
         // eslint-disable-next-line no-magic-numbers
         content = JSON.stringify( JSON.parse( content ), null, 2 );
       }
-      
+
       let parser = new CodeStudioLanguageParser( getParserForFileExtension( pathBrowserify.extname( path ).replace( ".", "" ) ) );
-      
+
       if ( !parser ) {
         parser = new CodeStudioLanguageParser( "plainText" );
       }
-      
+
       console.log( content );
       console.log( parser );
-      
-      editor._debugRenderParsedString( content, parser );
+
+      editor.renderParsedString( content, parser );
     } );
-    
-  }, [!!ref.current, path, formatJson] );
-  
+
+  }, [ !!ref.current, path, formatJson ] );
+
   return (
     <section className={"flex flex-col gap-2"}>
       <div className={"flex gap-1"}>
