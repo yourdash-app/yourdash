295f555703ae3d7b7c569c3c2a70c1e42577ba43
ewsgit <ewsgit@duck.com>
2024-01-04 10:31:29 +0000

    working: general WebDAV changes 4/1/24

--- packages/shared/xml/parseXmlTree.ts
+++ packages/shared/xml/parseXmlTree.ts
@@ -3,4 +3,38 @@
  * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
  */
 
-export default function parseXMLTree(unparsedTree: string) {}
+import XmlAttributeNode from "./xmlAttributeNode";
+import XmlElementNode from "./xmlElementNode";
+
+function parseAttributes({
+  unparsedElementNodeTag,
+}: {
+  unparsedElementNodeTag: string;
+}) {
+  const attributes = unparsedElementNodeTag.split(" ");
+
+  return attributes.map((attr) => {
+    const [attributeName, attributeValue] = attr.split("=");
+
+    return new XmlAttributeNode({});
+  });
+}
+
+function parseElementNode({
+  unparsedXMLTree,
+  nameSpaces,
+}: {
+  unparsedXMLTree: string;
+  nameSpaces: Map<string, string>;
+}) {
+  const [namespace, elementName] = unparsedXMLTree
+    .split("<")[1]
+    .split(" ")[0]
+    .split(":");
+
+  return new XmlElementNode({ currentNodeNamespace: namespace, nameSpaces });
+}
+
+export default function parseXMLTree(unparsedTree: string) {
+  const remainingString = unparsedTree;
+}
