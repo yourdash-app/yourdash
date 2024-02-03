588ce147d2481db902cd8e854250fe2fa725e53c
ewsgit <ewsgit@duck.com>
2024-02-03 19:57:49 +0000

    working: general working changes

--- packages/opengfx-engine/src/demo/2d/gfxts-one/src/main.ts
+++ packages/opengfx-engine/src/demo/2d/gfxts-one/src/main.ts
@@ -3,14 +3,13 @@
  * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
  */
 
-import initEngine from "opengfx"
-import loadObject from "../../../../runtime/core/object/loadObject.ts";
+import initEngine from "opengfx";
+// import loadObject from "../../../../runtime/core/object/loadObject.ts";
 // @ts-ignore
-import BOEING_MODEL from "./../public/boeing/Boeing_787.obj?raw"
+// import BOEING_MODEL from "./assets/boeing/Boeing_787.obj?raw";
 
-// @ts-ignore
-const engine = await initEngine( document.body as HTMLDivElement )
+await initEngine(document.body as HTMLDivElement);
 
-await loadObject( BOEING_MODEL, engine.screen, engine )
+// await loadObject(BOEING_MODEL, engine.screen, engine);
 
 // engine.setScene( new Scene( { id: "default_scene", objects: [] }, engine.screen ) )
---
+++ packages/opengfx-engine/src/runtime/index.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright ©2024 @Ewsgit and YourDash contributors.
+ * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
+ */
+
+export enum GFXEngineRuntime {
+  WGPU,
+  Canvas2D,
+}
+
+export default function getEngineRuntime(runtime: GFXEngineRuntime) {
+  switch (runtime) {
+    case GFXEngineRuntime.WGPU:
+      return import("./core/index.ts");
+    case GFXEngineRuntime.Canvas2D:
+      return import("./canvas/index.ts");
+  }
+}
