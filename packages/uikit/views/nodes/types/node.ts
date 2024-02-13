b39011729181bd79a44f0d214b53c7988e7da052
Ewsgit <ewsgit@duck.com>
2024-02-13 18:48:00 +0000

    working: working changes

--- packages/uikit/views/nodes/types/node.ts
+++ packages/uikit/views/nodes/types/node.ts
@@ -2,22 +2,26 @@ import { UUID } from "@yourdash/shared/core/uuid";
 
 export interface INode {
   type: string;
-  inputs: { [ key: string ]: any };
-  outputs: { [ key: string ]: any }
+  inputs: { [key: string]: any };
+  outputs: { [key: string]: any };
 }
 
 export interface INodeData<N extends INode, CustomData> {
-  id: UUID,
-  type: string,
-  inputs: { [ val in keyof N[ "inputs" ] ]: {
-    value: N[ "inputs" ][ val ],
-    points: { x: number, y: number }[],
-    from: INodeData<any, CustomData>
-  } },
-  outputs: { [ val in keyof N[ "inputs" ] ]: {
-    value: N[ "inputs" ][ val ],
-    points: { x: number, y: number }[],
-    to: INodeData<any, CustomData>
-  } },
-  data: CustomData
+  id: UUID;
+  type: string;
+  inputs: {
+    [val in keyof N["inputs"]]: {
+      value: N["inputs"][val];
+      points: { x: number; y: number }[];
+      from: INodeData<any, CustomData>;
+    };
+  };
+  outputs: {
+    [val in keyof N["inputs"]]: {
+      value: N["inputs"][val];
+      points: { x: number; y: number }[];
+      to: INodeData<any, CustomData>;
+    };
+  };
+  data: CustomData;
 }
