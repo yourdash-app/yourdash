9741426adad148bf0575b231c7a5ffb91a143304
Ewsgit <ewsgit@duck.com>
2024-02-14 13:55:22 +0000

    Add files via upload

---
+++ packages/opengfx-engine/runtime/three/index.ts
@@ -0,0 +1,8 @@
+import EngineRuntime from "../../engineRuntime";
+import { Engine } from "../core";
+
+export default class ThreeEngineRuntime extends EngineRuntime {
+  constructor(engine: Engine) {
+    super(engine)
+  }
+}
