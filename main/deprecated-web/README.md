ba5092a53cc215fe1541a7158a69d5ce0ecc26de
ewsgit <ewsgit@duck.com>
2024-05-27 22:30:59 +0100

    desktop: create basic desktop application and remove deprecated-web

--- main/deprecated-web/README.md
+++
@@ -1,4 +0,0 @@
-# YourDash - Web
-- The next-generation web client for YourDash
-
-TODO: add to this README with more information about the web client and how to develop it
---
+++ main/desktop/src/preload.ts
@@ -0,0 +1,7 @@
+/*
+ * Copyright ©2024 Ewsgit<https://github.com/ewsgit> and YourDash<https://github.com/yourdash> contributors.
+ * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
+ */
+
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
