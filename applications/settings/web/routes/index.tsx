4dff77cd0233a200d965cba7c577721492c08309
ewsgit <ewsgit@duck.com>
2024-06-02 21:53:57 +0100

    Settings: begin redesign of /app/a/settings/

--- applications/settings/web/routes/index.tsx
+++ applications/settings/web/routes/index.tsx
@@ -3,71 +3,91 @@
  * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
  */
 
+import Button from "@yourdash/uikit/components/button/button";
 import Card from "@yourdash/uikit/components/card/card";
 import Flex from "@yourdash/uikit/components/flex/flex";
 import Heading from "@yourdash/uikit/components/heading/heading";
 import Icon from "@yourdash/uikit/components/icon/icon";
 import { UKIcon } from "@yourdash/uikit/components/icon/iconDictionary";
+import SidebarToggleButton from "@yourdash/uikit/views/sidebar/SidebarToggleButton";
 import { FC } from "react";
 import styles from "./index.module.scss";
 import Text from "@yourdash/uikit/components/text/text";
 
 const IndexPage: FC = () => {
   return (
-    <div className={styles.page}>
-      <Heading
-        className={styles.title}
-        level={1}
-        text={"Home"}
-      />
-      <div className={styles.column}>
-        <Card>
+    <>
+      <div className={styles.page}>
+        <Flex
+          direction={"row"}
+          className={styles.titleContainer}
+        >
+          <SidebarToggleButton />
           <Heading
-            level={3}
-            className={styles.cardHeader}
-            text={"Personalise YourDash"}
+            className={styles.title}
+            level={1}
+            text={"Home"}
           />
-          <Flex
-            direction={"row"}
-            className={styles.themePreviewContainer}
-          >
-            <Card className={styles.themePreview}>Theme Preview 1</Card>
-            <Card className={styles.themePreview}>Theme Preview 2</Card>
-            <Card className={styles.themePreview}>Theme Preview 3</Card>
-            <Card className={styles.themePreview}>Theme Preview 4</Card>
-            <Card className={styles.themePreview}>Theme Preview 5</Card>
-          </Flex>
-        </Card>
-        <Card>
-          <Heading
-            level={3}
-            className={styles.cardHeader}
-            text={"Instance Software Updates"}
-          />
-          <Flex direction={"row"}>
-            <Icon
-              icon={UKIcon.CheckCircle}
-              size={"1.5rem"}
+        </Flex>
+        <div className={styles.column}>
+          <Card>
+            <Heading
+              level={3}
+              className={styles.cardHeader}
+              text={"Personalise YourDash"}
             />
-            <Text text={"You're all up to date!"} />
-          </Flex>
-        </Card>
+            <Flex
+              direction={"row"}
+              className={styles.themePreviewContainer}
+            >
+              <Card className={styles.themePreview}>Theme Preview 1</Card>
+              <Card className={styles.themePreview}>Theme Preview 2</Card>
+              <Card className={styles.themePreview}>Theme Preview 3</Card>
+              <Card className={styles.themePreview}>Theme Preview 4</Card>
+              <Card className={styles.themePreview}>Theme Preview 5</Card>
+            </Flex>
+          </Card>
+          <Card>
+            <Heading
+              level={3}
+              className={styles.cardHeader}
+              text={"Instance Software Updates"}
+            />
+            <Flex direction={"row"}>
+              <Icon
+                icon={UKIcon.CheckCircle}
+                size={"1.5rem"}
+              />
+              <Text text={"You're all up to date!"} />
+            </Flex>
+            <Flex direction={"column"}>
+              <Text text={"Last checked 2 days ago"} />
+              <Button
+                text={"Check for updates"}
+                onClick={() => {
+                  return 0;
+                }}
+              />
+              <Text text={"currently version: 1.0.0"} />
+            </Flex>
+          </Card>
+        </div>
+        <div className={styles.column}>
+          <Card>
+            <Heading
+              level={3}
+              className={styles.cardHeader}
+              text={"Manage Wallpaper"}
+            />
+            <Flex direction={"row"}>
+              <Card>Previous Image 1</Card>
+              <Card>Previous Image 2</Card>
+              <Card>+</Card>
+            </Flex>
+          </Card>
+        </div>
       </div>
-      <div className={styles.column}>
-        <Card>
-          <Heading
-            level={3}
-            className={styles.cardHeader}
-            text={"Manage Wallpaper"}
-          />
-          <Flex direction={"row"}>
-            <Card>Previous Image 1</Card>
-            <Card>Previous Image 2</Card>
-            <Card>+</Card>
-          </Flex>
-        </Card>
-      </div>
-    </div>
+    </>
   );
 };
 
--- applications/settings/web/settingsLayout.tsx
+++ applications/settings/web/settingsLayout.tsx
@@ -4,7 +4,11 @@
  */
 
 import { UKIcon } from "@yourdash/chiplet/components/icon/iconDictionary";
-import SideBar, { SIDEBAR_ITEM_TYPE, SIDEBAR_STATE } from "@yourdash/chiplet/components/sideBar/SideBar";
+import Button from "@yourdash/uikit/components/button/button";
+import Heading from "@yourdash/uikit/components/heading/heading";
+import Separator from "@yourdash/uikit/components/separator/separator";
+import SideBar from "@yourdash/uikit/views/sideBar/SideBar";
+import SidebarContainer from "@yourdash/uikit/views/sidebar/SidebarContainer";
 import * as React from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
@@ -12,55 +16,85 @@ const SettingsLayout: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <main className={"grid grid-cols-1 grid-rows-[auto,1fr] md:grid-rows-none md:grid-cols-[auto,1fr] h-full w-full bg-bg"}>
-      <SideBar
-        defaultState={SIDEBAR_STATE.NormalMinimised}
-        title={"Settings"}
-        items={[
-          {
-            type: SIDEBAR_ITEM_TYPE.Button,
-            icon: UKIcon.Home,
-            label: "Home",
-            onClick() {
-              navigate("/app/a/settings/");
-            },
-          },
-          {
-            type: SIDEBAR_ITEM_TYPE.Button,
-            icon: UKIcon.Paintbrush,
-            label: "Personalization",
-            onClick() {
-              navigate("/app/a/settings/personalization");
-            },
-          },
-          {
-            type: SIDEBAR_ITEM_TYPE.Button,
-            icon: UKIcon.Login,
-            label: "Login sessions",
-            onClick() {
-              navigate("/app/a/settings/session");
-            },
-          },
-          {
-            type: SIDEBAR_ITEM_TYPE.Button,
-            icon: UKIcon.Accessibility,
-            label: "Accessibility",
-            onClick() {
-              navigate("/app/a/settings/accessibility");
-            },
-          },
-          {
-            type: SIDEBAR_ITEM_TYPE.Button,
-            icon: UKIcon.Tools,
-            label: "Admin tools",
-            onClick() {
-              navigate("/app/a/settings/admin");
-            },
-          },
-        ]}
-      />
+    // <main className={"grid grid-cols-1 grid-rows-[auto,1fr] md:grid-rows-none md:grid-cols-[auto,1fr] h-full w-full bg-bg"}>
+    //   <SideBar
+    //     defaultState={SIDEBAR_STATE.NormalMinimised}
+    //     title={"Settings"}
+    //     items={[
+    //       {
+    //         type: SIDEBAR_ITEM_TYPE.Button,
+    //         icon: UKIcon.Home,
+    //         label: "Home",
+    //         onClick() {
+    //           navigate("/app/a/settings/");
+    //         },
+    //       },
+    //       {
+    //         type: SIDEBAR_ITEM_TYPE.Button,
+    //         icon: UKIcon.Paintbrush,
+    //         label: "Personalization",
+    //         onClick() {
+    //           navigate("/app/a/settings/personalization");
+    //         },
+    //       },
+    //       {
+    //         type: SIDEBAR_ITEM_TYPE.Button,
+    //         icon: UKIcon.Login,
+    //         label: "Login sessions",
+    //         onClick() {
+    //           navigate("/app/a/settings/session");
+    //         },
+    //       },
+    //       {
+    //         type: SIDEBAR_ITEM_TYPE.Button,
+    //         icon: UKIcon.Accessibility,
+    //         label: "Accessibility",
+    //         onClick() {
+    //           navigate("/app/a/settings/accessibility");
+    //         },
+    //       },
+    //       {
+    //         type: SIDEBAR_ITEM_TYPE.Button,
+    //         icon: UKIcon.Tools,
+    //         label: "Admin tools",
+    //         onClick() {
+    //           navigate("/app/a/settings/admin");
+    //         },
+    //       },
+    //     ]}
+    //   />
+    //   <Outlet />
+    // </main>
+    <SidebarContainer showSidebarByDefault>
+      <SideBar>
+        <Heading
+          level={2}
+          text={"Settings"}
+        />
+        <Separator direction={"column"} />
+        <Button
+          text={"Home"}
+          onClick={() => navigate("/app/a/settings/")}
+        />
+        <Button
+          text={"Personalisation"}
+          onClick={() => navigate("/app/a/settings/personalization")}
+        />
+        <Button
+          text={"Login Sessions"}
+          onClick={() => navigate("/app/a/settings/session")}
+        />
+        <Button
+          text={"Administrator Tools"}
+          onClick={() => navigate("/app/a/settings/admin")}
+        />
+        <Button
+          text={"Developer Tools"}
+          onClick={() => navigate("/app/a/settings/developer")}
+        />
+      </SideBar>
       <Outlet />
-    </main>
+    </SidebarContainer>
   );
 };
 
