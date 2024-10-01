6786257fe245e236760e9f42c9c23fecd21cef9f
Ewsgit <ewsgit@duck.com>
2024-10-01 19:04:48 +0100

    CoreRuntime: made a bun process to watch the main process and restart on crashes

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,6 +1,6 @@
 /*
- * Copyright ©2024 Ewsgit<https://github.com/ewsgit> and YourDash<https://github.com/yourdash> contributors.
- * YourDash is licensed under the MIT License. (https://ewsgit.mit-license.org)
+ * Copyright ©2024 Ewsgit <https://ewsgit.uk> and YourDash <https://yourdash.ewsgit.uk> contributors.
+ * YourDash is licensed under the MIT License. (https://mit.ewsgit.uk)
  */
 
 /**
@@ -10,18 +10,53 @@
  *  - https://ydsh.pages.dev
  */
 
-import core from "./core/core.js";
+import Bun from "bun";
 
-try {
-  // Start the YourDash Instance
-  core.__internal__startInstance();
-} catch (err) {
-  core.log.error("core", `An error occurred with YourDash startup: ${err}`);
+async function initProcess() {
+  console.log("Starting YourDash process...");
+
+  const yourdashProcess = Bun.spawn(["bun", "run", "dev-standalone", ...process.argv.slice(2)], {
+    stdin: "pipe",
+    stderr: "pipe",
+    stdout: "pipe",
+    cwd: process.cwd(),
+  });
+
+  const stdoutDecoder = new TextDecoder();
+  const stdinEncoder = new TextEncoder();
+
+  for await (const chunk of yourdashProcess.stdout) {
+    let decodedChunk = stdoutDecoder.decode(chunk);
+
+    if (decodedChunk.includes("oh no: Bun has crashed. This indicates a bug in Bun, not your code.")) {
+      yourdashProcess.kill();
+
+      initProcess().then((r) => {
+        console.log("process restarted");
+      });
+    }
+
+    process.stdout.write(decodedChunk);
+  }
+
+  for await (const chunk of yourdashProcess.stderr) {
+    let decodedChunk = stdoutDecoder.decode(chunk);
+
+    process.stderr.write("stderr " + decodedChunk);
+  }
+
+  process.stdin.on("data", (chunk: any) => {
+    yourdashProcess.stdin.write(stdinEncoder.encode(chunk));
+    yourdashProcess.stdin.flush();
+  });
+
+  process.on("beforeExit", () => {
+    yourdashProcess.kill();
+  });
+
+  yourdashProcess.exited.then((processExitCode) => {
+    console.log(`YourDash instance process exited with exit code ${processExitCode}`);
+  });
 }
 
-/*
-// if we are not running in development mode, start the check for updates task
-if ( !PROCESS_ARGUMENTS.dev ) {
-  scheduleBackendUpdateChecker();
-}
-*/
+initProcess();
