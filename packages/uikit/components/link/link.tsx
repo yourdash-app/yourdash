c5a4c59d7307eb36c9b981f1b0d06894a19f522e
ewsgit <ewsgit@duck.com>
2024-10-23 11:27:03 +0100

    UIKit: add buttonCombo.tsx and fix gap for buttonWithIcon

--- packages/uikit/components/link/link.tsx
+++ packages/uikit/components/link/link.tsx
@@ -4,16 +4,24 @@
  */
 
 import clippy from "@yourdash/shared/web/helpers/clippy.ts";
+import Icon from "../icon/icon.tsx";
+import { UKIcon } from "../icon/iconDictionary.ts";
 import styles from "./link.module.scss";
 import { FC } from "react";
 
-const Link: FC<{ text: string; to: string; className?: string }> = (props) => {
+const Link: FC<{ text: string; to: string; className?: string; hideLinkIcon: boolean }> = (props) => {
   return (
     <a
       href={props.to}
       className={clippy(styles.component, props.className)}
     >
       {props.text}
+      {!props.hideLinkIcon && (
+        <Icon
+          icon={UKIcon.Link}
+          className={styles.icon}
+        />
+      )}
     </a>
   );
 };
